60178ff7263f8e2402ff7c5c7daea37cde68e607
Pepe <jotego@gmail.com>
2017-08-30 07:28:50 +0200

    wip

--- ver/jt_gng/go.sh
+++ ver/jt_gng/go.sh
@@ -3,6 +3,7 @@
 DUMP=NODUMP
 CHR_DUMP=NOCHR_DUMP
 RAM_INFO=NORAM_INFO
+LOCALROM=
 REPS=1
 
 while [ $# -gt 0 ]; do
@@ -30,13 +31,33 @@ while [ $# -gt 0 ]; do
 		shift
 		continue
 	fi
+	if [ "$1" = "-local" ]; then
+		LOCALROM=-DLOCALROM
+		if [ ! -e gng_test.s ]; then
+			echo "Local ROM requested but no gng_test.s file found for assembly"
+			exit 1
+		fi
+		echo Local ROM enabled
+		shift
+		continue
+	fi	
 	echo "Unknown option $1"
 	exit 1
 done
+
+if [ LOCALROM != "" ]; then
+	if [ ! -e 10n.hex ]; then touch 10n.hex; fi
+	if [ ! -e 13n.hex ]; then touch 13n.hex; fi
+	if ! lwasm gng_test.s --output=gng_test.bin --list=gng_test.lst --format=raw; then
+		exit 1
+	fi
+	OD="od -t x1 -A none -v -w1"
+	$OD gng_test.bin > 8n.hex
+fi
 
 iverilog jt_gng_test.v \
 	../../hdl/*.v \
 	../../modules/mc6809/{mc6809.v,mc6809i.v} \
 	-s jt_gng_test -o sim \
-	-D$DUMP -D$CHR_DUMP -D$RAM_INFO -DREPS=$REPS\
+	-D$DUMP -D$CHR_DUMP -D$RAM_INFO -DREPS=$REPS $LOCALROM\
 && sim -lxt
