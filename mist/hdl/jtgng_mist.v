268d52dc4dfdd3403119e25540a0b5e65fdb0c1d
Jose Tejada <jotego@gmail.com>
2019-01-10 23:10:45 +0100

    wip

--- mist/hdl/jtgng_mist.v
+++ mist/hdl/jtgng_mist.v
@@ -187,7 +187,7 @@ jtgng_game game(
     .romload_addr( romload_addr  ),
     .romload_data( romload_data  ),
     // DEBUG
-    .enable_char ( ~status[5]    ),
+    .enable_char ( ~status[3]    ),
     .enable_scr  ( ~status[3]    ),
     .enable_obj  ( ~status[4]    ),
     // DIP switches
@@ -225,7 +225,7 @@ jtgng_vga vga_conv (
     .blue       ( blue          ),
     .LHBL       ( LHBL          ),
     .LVBL       ( LVBL          ),
-    .en_mixing  ( 1'b0 ), //~status[5]    ),
+    .en_mixing  ( ~status[5]    ),
     .vga_red    ( GNG_R[5:1]    ),
     .vga_green  ( GNG_G[5:1]    ),
     .vga_blue   ( GNG_B[5:1]    ),
