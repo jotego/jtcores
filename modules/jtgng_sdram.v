4f32c0887a2cfb6fa2a39214a6cf6fed12c6fc60
jotego <jotego@analog>
2019-02-27 18:04:17 +0100

    Trying to change the SDRAM to burst-2 mode. Broken

--- modules/jtgng_sdram.v
+++ modules/jtgng_sdram.v
@@ -25,7 +25,7 @@ module jtgng_sdram(
     output              loop_rst,  
     input               autorefresh,
     input               read_req,    // read strobe
-    output reg  [15:0]  data_read,
+    output reg  [31:0]  data_read,
     input       [21:0]  sdram_addr,
     // ROM-load interface
     input               downloading,
@@ -72,7 +72,7 @@ reg [13:0] wait_cnt;
 reg [2:0] cnt_state, init_state;
 reg       initialize;
 
-reg write_cycle=1'b0;
+reg write_cycle=1'b0, read_cycle=1'b0;
 
 assign loop_rst = initialize;
 
@@ -81,6 +81,18 @@ reg last_read_req;
 always @(posedge clk) last_read_req <= read_req;
 wire readon  = !downloading && ((read_req!=last_read_req) /*|| autorefresh*/);
 wire writeon = downloading && prog_we;
+
+reg downloading_last;
+reg set_burst, burst_done;
+
+always @(posedge clk) 
+    if(rst) begin
+        set_burst <= 1'b0;
+    end else begin
+        downloading_last <= downloading;
+        if( !downloading && downloading_last) set_burst <= 1'b1;
+        if( burst_done ) set_burst <= 1'b0;
+    end
 
 reg autorefresh_cycle;
 reg [21:0] last_sdram_addr;
@@ -94,12 +106,10 @@ always @(posedge clk)
         SDRAM_CMD <= CMD_NOP;
         wait_cnt   <= 14'd9750; // wait for 100us
         initialize <= 1'b1;
-        init_state <= 3'd0;
+        init_state <= 3'd0;        
         // Main loop
+        burst_done <= 1'b0;
         cnt_state  <= 3'd3; //Starts after the precharge
-            // 0,1,4,5,6,7 fails
-            // 3 SCROLL is chunky
-            // 2 SCROLL very chunky
     end else if( initialize ) begin
         if( |wait_cnt ) begin
             wait_cnt <= wait_cnt-14'd1;
@@ -117,8 +127,15 @@ always @(posedge clk)
                     wait_cnt  <= 14'd10;
                 end
                 3'd2: begin
-                    SDRAM_CMD <= CMD_LOAD_MODE;
+                    SDRAM_CMD <= CMD_LOAD_MODE;                    
                     SDRAM_A   <= 13'b00_1_00_010_0_000; // CAS Latency = 2
+                    `ifdef SIMULATION
+                    `ifndef LOADROM
+                        // Start directly with burst mode on simulation
+                        // if the ROM load process is not being simulated
+                        SDRAM_A   <= 13'b00_1_00_010_0_001; // CAS Latency = 2
+                    `endif
+                    `endif
                     wait_cnt  <= 14'd2;
                 end
                 3'd3: begin
@@ -141,21 +158,33 @@ always @(posedge clk)
         end
         3'd0: begin // activate or refresh
             write_data  <= prog_data;
+            write_cycle       <= 1'b0;
+            read_cycle        <= 1'b0;
+            autorefresh_cycle <= 1'b0;
+            if( read_cycle) data_read[31:16] <= SDRAM_DQ;
             {SDRAM_DQMH, SDRAM_DQML } <= 2'b00;
-            if( writeon ) begin
-                SDRAM_CMD <= CMD_ACTIVATE;
-                { SDRAM_A, col_addr } <= prog_addr[21:0];
-                autorefresh_cycle <= 1'b0;
-                write_cycle       <= 1'b1;
-                {SDRAM_DQMH, SDRAM_DQML } <= prog_mask;
-            end 
-            if( readon ) begin
-                last_sdram_addr <= sdram_addr;
-                SDRAM_CMD <= 
-                    refresh_ok ? CMD_AUTOREFRESH : CMD_ACTIVATE;
-                { SDRAM_A, col_addr } <= sdram_addr;                
-                autorefresh_cycle <= refresh_ok; 
-                write_cycle       <= 1'b0;
+            if( set_burst ) begin
+                SDRAM_CMD <= CMD_LOAD_MODE;                    
+                SDRAM_A   <= 13'b00_1_00_010_0_001; // CAS Latency = 2, burst = 2
+                burst_done <= 1'b1;
+            end else begin
+                SDRAM_CMD <= CMD_NOP;
+                if( writeon ) begin
+                    SDRAM_CMD <= CMD_ACTIVATE;
+                    { SDRAM_A, col_addr } <= prog_addr[21:0];
+                    autorefresh_cycle <= 1'b0;
+                    write_cycle       <= 1'b1;
+                    {SDRAM_DQMH, SDRAM_DQML } <= prog_mask;
+                end 
+                if( readon ) begin
+                    last_sdram_addr <= sdram_addr;
+                    SDRAM_CMD <= 
+                        refresh_ok ? CMD_AUTOREFRESH : CMD_ACTIVATE;
+                    { SDRAM_A, col_addr } <= sdram_addr;                
+                    autorefresh_cycle <= refresh_ok;
+                    read_cycle        <= ~refresh_ok;
+                    write_cycle       <= 1'b0;
+                end
             end
         end
         3'd2: begin // set read/write            
@@ -166,9 +195,10 @@ always @(posedge clk)
                 autorefresh_cycle ? CMD_NOP : CMD_READ;
         end
         3'd5: begin
-            if(!write_cycle && !autorefresh_cycle) data_read <= SDRAM_DQ;
-            write_cycle       <= 1'b0;
-            autorefresh_cycle <= 1'b0;
+            if( read_cycle) begin
+                data_read[15:0] <= SDRAM_DQ;
+            end
+            SDRAM_CMD <= CMD_NOP;
         end
         endcase
     end
