bbdab663f8f62e0e6bc590ed6ee6b096fb6dd694
jotego <jotego@analog>
2019-02-27 16:58:38 +0100

    1943: before changing the ROM/SDRAM modules

--- 1943/hdl/jt1943_scroll.v
+++ 1943/hdl/jt1943_scroll.v
@@ -34,7 +34,6 @@ module jt1943_scroll(
     input              flip,
     input       [ 7:0] din,
     input              wr_n,
-
     input              pause,
     // Palette PROMs D1, D2
     input   [7:0]      prog_addr,
@@ -56,46 +55,28 @@ parameter SIMFILE_MSB="", SIMFILE_LSB="";
 parameter AS8MASK = 1'b1;
 
 wire [8:0] Hfix = H + HOFFSET; // Corrects pixel output offset
-reg  [ 8:0] HS;
+reg  [ 4:0] HS;
 reg  [ 7:0] VF, SV, SH, PIC;
 wire [ 7:0] HF = {8{flip}}^Hfix[7:0]; // SCHF2_1-8
 reg  [15:0] hpos; // called "SP" on the schematics
 
 wire H7 = (~Hfix[8] & (~flip ^ HF[6])) ^HF[7];
-
-reg [2:0] HSaux;
-
-
-`ifdef TESTSCR1
-//reg [15:0] hpos_test;
-always @(posedge LVBL or posedge rst)
-    if( rst )
-        hpos <= 'd768;
-    else
-        hpos <= hpos + 16'd8;
-`endif
-
 wire [9:0] SCHF = { HF[6]&~Hfix[8], ~Hfix[8], H7, HF[6:0] }; // SCHF30~21
 
-always @(posedge clk) if(cen6) begin
-    VF <= {8{flip}}^V128;
-    SV <= VF + ( pause ? 8'd0 : vpos );
-    {PIC, SH } <= hpos + { {7{SCHF[9]}},SCHF[8:0] };
-    map_addr <= { PIC, SH[7:5], SV[7:5] };
-    HS[8:3] <= SCHF[8:3];
-    HS[2:0] <= SCHF[2:0] ^ {3{flip}};
+always @(*) begin
+    VF = {8{flip}}^V128;
+    SV = VF + vpos;
+    {PIC, SH }  = hpos + { {6{SCHF[9]}},SCHF };
 end
 
-
-/* H1  = C18
-   H8  = D19
-   H16 = C20 
-   H32 = D20
-   H64 = C21
-   H128= D21
-   H256= C22
-
-*/
+always @(posedge clk) if(cen6) begin
+    // always update the map at the same pixel count
+    if( SH[2:0]==3'd0 ) begin
+        HS[4:3] <= SH[4:3];
+        map_addr <= { PIC, SH[7:5], SV[7:5] };
+    end
+    HS[2:0] <= SH[2:0] ^ {3{flip}};
+end
 
 always @(posedge clk) 
     if( rst ) begin
@@ -111,19 +92,21 @@ wire [7:0] dout_low  = map_data[15:8];
 reg scr_hflip;
 reg [7:0] addr_lsb;
 
-reg [4:0] scr_attr0, scr_attr1;
+reg [4:0] scr_attr0;
+wire [4:0] scr_attr1;
+assign scr_attr1 = scr_attr0;
 
 // Set input for ROM reading
 always @(posedge clk) if(cen6) begin
-    if( HS[2:0]==3'd1 ) begin // dout_high/low data corresponds to this tile
+    if( HS[2:0]==3'b1 ) begin // dout_high/low data corresponds to this tile
             // from HS[2:0] = 1,2,3...0. because RAM output is latched
-        scr_attr1 <= scr_attr0;
         scr_attr0 <= dout_high[6:2];
         scr_addr[16:1] <= {   dout_high[0] & AS8MASK, dout_low, // AS
                         HS[4:3]^{2{dout_high[6]}} /*scr_hflip*/, 
-                        {5{dout_high[7] /*vflip*/}}^VF[4:0] }; /*vert_addr*/
+                        {5{dout_high[7] /*vflip*/}}^SV[4:0] }; /*vert_addr*/
+        scr_addr[0] <= HS[2]^dout_high[6];
     end
-    scr_addr[0] <= ~HS[2]^dout_high[6];
+    else if(HS[2:0]==3'b101 ) scr_addr[0] <= HS[2]^scr_attr0[4];
 end
 
 // Draw pixel on screen
@@ -134,7 +117,7 @@ always @(posedge clk) if(cen6) begin
     // new tile starts 8+5=13 pixels off
     // 8 pixels from delay in ROM reading
     // 4 pixels from processing the x,y,z and attr info.
-    if( HS[1:0]==2'd2 ) begin
+    if( HS[1:0]==2'd1 ) begin
             { z,y,x,w } <= scrom_data;     
             scr_hflip   <= scr_attr1[4] ^ flip; // must be ready when z,y,x are.
             scr_attr2   <= scr_attr1[3:0];
