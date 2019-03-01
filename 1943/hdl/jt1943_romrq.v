42596fd72b70f876628e88d1a50437c719e0602b
jotego <jotego@analog>
2019-03-01 11:51:25 +0100

    1943: Cache size set to 64 bytes. SDRAM usage 13%

--- 1943/hdl/jt1943_romrq.v
+++ 1943/hdl/jt1943_romrq.v
@@ -29,27 +29,47 @@ module jt1943_romrq #(parameter AW=18, DW=8, INVERT_A0=0 )(
     output reg [DW-1:0] dout
 );
 
-reg [AW-1:0] cached_addr;
+reg [AW-1:0] cached_addr0;
+reg [AW-1:0] cached_addr1;
+reg [31:0]   cached_data0;
+reg [31:0]   cached_data1;
+reg deleterus;
 reg [1:0]    subaddr;
-reg [31:0]   cached_data;
 reg init;
+reg hit0, hit1;
 
 always @(*) begin
     case(DW)
         8:  addr_req = {addr[AW-1:2],2'b0};
         16: addr_req = {addr[AW-1:1],1'b0};
         32: addr_req = addr;
-    endcase 
-    req = init || ((addr_req !== cached_addr) && addr_ok);
+    endcase
+    hit0 = addr_req === cached_addr0;
+    hit1 = addr_req === cached_addr1;
+    req = init || ( !(hit0 || hit1) && addr_ok && !we);
 end
 
 always @(posedge clk) 
     if( rst ) begin
         init <= 1'b1;
+        deleterus <= 1'b0;
     end else if(cen) begin
         if( we ) begin
-            cached_data <= din;
-            cached_addr <= addr_req;
+            if( init ) begin
+                cached_data0 <= din;
+                cached_addr0 <= addr_req;
+                cached_data1 <= din;
+                cached_addr1 <= addr_req;
+            end else begin
+                if( deleterus ) begin
+                    cached_data1 <= din;
+                    cached_addr1 <= addr_req;
+                end else begin
+                    cached_data0 <= din;
+                    cached_addr0 <= addr_req;
+                end
+                deleterus <= ~deleterus;
+            end
             init        <= 1'b0;
         end
     end
@@ -61,22 +81,24 @@ always @(*) begin
         subaddr[0] <=  addr[0]; 
 end
 
+wire [31:0] data_mux = hit0 ? cached_data0 : cached_data1;
+
 generate
     if(DW==8) begin
         always @(posedge clk)
         if(!req) case( subaddr )
-            2'd0: dout <= cached_data[ 7: 0];
-            2'd1: dout <= cached_data[15: 8];
-            2'd2: dout <= cached_data[23:16];
-            2'd3: dout <= cached_data[31:24];
+            2'd0: dout <= data_mux[ 7: 0];
+            2'd1: dout <= data_mux[15: 8];
+            2'd2: dout <= data_mux[23:16];
+            2'd3: dout <= data_mux[31:24];
         endcase
     end else if(DW==16) begin
         always @(posedge clk)
         if(!req) case( subaddr[0] )
-                1'd0: dout = cached_data[15:0];
-                1'd1: dout = cached_data[31:16];
+                1'd0: dout = data_mux[15:0];
+                1'd1: dout = data_mux[31:16];
         endcase
-    end else always @(*) dout = cached_data;
+    end else always @(*) dout = data_mux;
 endgenerate
 
 
