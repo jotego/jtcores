5662cab1b12eb7f8a17808d09e65f98719348863
jotego <jotego@gmail.com>
2019-05-07 12:46:32 +0200

    minor

--- 1943/ver/game/go.sh
+++ 1943/ver/game/go.sh
@@ -26,5 +26,6 @@ done
 
 
 # Generic simulation script from JTFRAME
+# ROM LENGTH 887808
 ../../../modules/jtframe/bin/sim.sh $MIST -d GAME_ROM_LEN=887808 -sysname 1943 \
     -modules ../../../modules $*
