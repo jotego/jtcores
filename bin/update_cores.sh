5bfe9d1ec81759b2138eb252652e51e9b05dfc75
Jose Tejada <jotego@gmail.com>
2019-08-23 08:45:00 +0200

    Initial MiSTer support for jtcore compilation setup

--- bin/update_cores.sh
+++ bin/update_cores.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+MIST=mist
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "-mister")
+            MIST=mister;;
+        "-mist")
+            MIST=mist;;
+        *)  echo "ERROR: Unknown option $1";
+            exit 1;;
+    esac
+    shift
+done
+
 cores="1942 1943 gng commando"
 
-(for i in $cores; do echo $i; done) | parallel jtcore 
+(for i in $cores; do echo $i; done) | parallel jtcore $MIST
