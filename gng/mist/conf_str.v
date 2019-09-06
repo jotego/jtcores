cf4c8e3ba88eec2d463fb07e911559eef6ac5893
Jose Tejada <jotego@gmail.com>
2019-09-06 08:16:48 +0200

    Commando fix release

--- gng/mist/conf_str.v
+++ gng/mist/conf_str.v
@@ -3,3 +3,5 @@
 `define CONF_STR \
     "OGH,Difficulty,normal,easy,hard,very hard;", \
     "OIJ,Lives,3,4,5,7;",
+`define CORE_KEYMAP \
+    "J,Button 1,Button 2,Button 3,1P Start,2P Start,Coin,Pause;",
