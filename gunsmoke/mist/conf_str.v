4ffbca2e8c949659a5cee94dbb228cfbe1cc756d
Jose Tejada <jotego@gmail.com>
2019-09-11 11:50:52 +0200

    New ROM

--- gunsmoke/mist/conf_str.v
+++ gunsmoke/mist/conf_str.v
@@ -3,7 +3,7 @@
 `define CORE_OSD \
     "OGH,Difficulty,Normal,Easy,Hard,Very hard;", \
     "OIJ,Bonus,20k & every 100k,20k and 100k only,20k and every 80k,20k and every 100k;", \
-    "OK,Demo sound,Yes,No;", \
-    "OL,Continue play,Yes,No;",
+    "OK,Demo sound,No,Yes;", \
+    "OL,Continue play,No,Yes;",
 `define CORE_KEYMAP \
     "J,Button 1,Button 2,Button 3,1P Start,2P Start,Coin,Credits;",
