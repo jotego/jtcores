6094ef22ef99d7beda8993d1d9e43da16e0d312f
Jose Tejada <jotego@gmail.com>
2019-10-05 12:56:52 +0200

    minor

--- bin/jtsdram.sh
+++ bin/jtsdram.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+MIN=0
+MAX=11000
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -min)
+            shift
+            MIN=$1;;
+        -max)
+            shift
+            MAX=$1;;
+        *)
+            echo "Unknown parameter"
+            exit 1;;
+    esac
+    shift
+done
+
 for DELAY in 0 260 520 729 1041 1250 1475 1736 1996 2256 2500 2734 2994 3255 3515 3750 3993 4253 4513 4774 5000 5208 5520 5729 5989 6250 6510 6770 6979 7291 7500 7725 7986 8246 8506 8750 8984 9244 9505 9765 10000 10243 10329;
 do
+    if [[ $DELAY -lt $MIN || $DELAY -gt $MAX ]]; then
+        continue
+    fi
     echo Using ${DELAY}ps delay
     jtcore 1943 -mr -d SDRAM_SHIFT=\\\"$DELAY\\\"
     OUTPUT=$JTGNG_ROOT/1943/mister/output_files/jt1943.rbf
