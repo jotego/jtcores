a82d185ed6841502a8467f5f26ec909fef386aed
neuro_999@hotmail.com <neuro_999@hotmail.com>
2019-12-03 13:29:26 +0100

    Cores de 1942,1943,commando,ghost and goblins, gunsmoke y vulgus para Cyclone5 y Unamiga

---
+++ vulgus/unamiga/jtvulgus.tcl
@@ -0,0 +1,8 @@
+set_global_assignment -name VERILOG_MACRO "CORENAME=\"JTvulgus\""
+set_global_assignment -name VERILOG_MACRO "VERTICAL_SCREEN=1"
+set_global_assignment -name VERILOG_MACRO "HAS_TESTMODE=1"
+set_global_assignment -name VERILOG_MACRO "GAMETOP=jtvulgus_game"
+set_global_assignment -name VERILOG_MACRO "ZX2TOP=jtvulgus_mist"
+
+set_global_assignment -name QIP_FILE ../../modules/t80/T80.qip
+set_global_assignment -name QIP_FILE ../../modules/jt12/jt49/hdl/jt49.qip
