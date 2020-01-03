5dbdb285a4305f9df6936ba7dba35c26f4592f10
Jose Tejada <jotego@gmail.com>
2020-01-03 16:37:11 +0100

    3rd January release

--- cores/1942/mister/jt1942.tcl
+++ cores/1942/mister/jt1942.tcl
@@ -1,3 +1,4 @@
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
 set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
+set_global_assignment -name VERILOG_MACRO "MISTER_VIDEO_MIXER=1"
 
