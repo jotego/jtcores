18343a8e37caba70c54875383abf857968cb0910
Jose Tejada <jotego@gmail.com>
2020-01-06 19:46:14 +0100

    TCL files

--- cores/1943/mister/jt1943.tcl
+++ cores/1943/mister/jt1943.tcl
@@ -1,5 +1,4 @@
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
 set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
 set_global_assignment -name VERILOG_MACRO "AVATARS=1"
-set_global_assignment -disable -name VERILOG_MACRO "OSD_NOBCK=<None>"
 set_global_assignment -name SEED 4
--- cores/gunsmoke/mister/jtgunsmoke.tcl
+++ cores/gunsmoke/mister/jtgunsmoke.tcl
@@ -1,3 +1,3 @@
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
 set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
 
