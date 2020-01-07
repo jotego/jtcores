74006eab3b5c842849e10286e8a1576c5517daa3
Jose Tejada <jotego@gmail.com>
2020-01-07 08:24:22 +0100

    Selects the right MiSTer scan doubler

--- cores/btiger/hdl/jtbtiger.tcl
+++ cores/btiger/hdl/jtbtiger.tcl
@@ -2,12 +2,12 @@ set_global_assignment -name VERILOG_MACRO "CORENAME=\"JTBTIGER\""
 set_global_assignment -name VERILOG_MACRO "GAMETOP=jtbtiger_game"
 set_global_assignment -name VERILOG_MACRO "JT12=1"
 set_global_assignment -name VERILOG_MACRO "AVATARS=1"
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
+set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
 
 # OSD options
 set_global_assignment -name VERILOG_MACRO "HAS_TESTMODE=1"
 set_global_assignment -name VERILOG_MACRO "JOIN_JOYSTICKS=1"
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
 
 set_global_assignment -name VERILOG_FILE ../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
 set_global_assignment -name VERILOG_FILE ../../modules/jt12/hdl/mixer/jt12_mixer.v
--- cores/btiger/mister/jtbtiger.tcl
+++ cores/btiger/mister/jtbtiger.tcl
@@ -1,2 +1,3 @@
-set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
-set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
+# set MiSTer scan doubler
+set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=4"
+set_global_assignment -name VERILOG_MACRO "MISTER_VIDEO_MIXER=1"
