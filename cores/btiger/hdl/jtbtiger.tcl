26baa371ede2ff57b04685ad56fe2ce6736e470a
neuro_999@hotmail.com <neuro_999@hotmail.com>
2020-01-08 13:06:21 +0100

    Modificada la ruta absoluta de los ficheros que apuntan a modules, añadiendo un ../ mas.

--- cores/btiger/hdl/jtbtiger.tcl
+++ cores/btiger/hdl/jtbtiger.tcl
@@ -9,6 +9,6 @@ set_global_assignment -name VERILOG_MACRO "HAS_TESTMODE=1"
 set_global_assignment -name VERILOG_MACRO "JOIN_JOYSTICKS=1"
 set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
 
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/hdl/mixer/jt12_mixer.v
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/hdl/mixer/jt12_mixer.v
  
