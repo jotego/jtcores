cdbe6606b0405d82edb274c1b1a514efdd85eff5
neurorulez <neuro_999@hotmail.com>
2020-01-12 09:49:48 +0100

    Modificada la ruta absoluta de los ficheros que apuntan a modules, añadiendo un ../ mas. (#37)

--- cores/1943/hdl/jt1943.tcl
+++ cores/1943/hdl/jt1943.tcl
@@ -5,5 +5,5 @@ set_global_assignment -name VERILOG_MACRO "GAMETOP=jt1943_game"
 set_global_assignment -name VERILOG_MACRO "JT12=1"
 set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
 
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/hdl/mixer/jt12_mixer.v
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/hdl/mixer/jt12_mixer.v
