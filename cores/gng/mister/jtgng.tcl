453f0e13ff147ecf67d0d9732765521ba7cce94b
Jose Tejada <jotego@gmail.com>
2020-01-18 14:24:47 +0100

    Fixed video width

--- cores/gng/mister/jtgng.tcl
+++ cores/gng/mister/jtgng.tcl
@@ -7,3 +7,6 @@ set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=4"
 set_global_assignment -name VERILOG_MACRO "MISTER_VIDEO_MIXER=1"
 set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
 set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
+
+# best 8: -3.076
+set_global_assignment -name SEED 9
