5b4d12d686913e8cc77662764d399bd036f4ba1a
Jose Tejada <jotego@gmail.com>
2020-04-15 16:21:30 +0200

    wip

---
+++ cores/sectionz/mister/jtsectionz.tcl
@@ -0,0 +1,5 @@
+set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=4"
+set_global_assignment -name VERILOG_MACRO "MISTER_VIDEO_MIXER=1"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=256"
+set_global_assignment -name VERILOG_MACRO "VIDEO_HEIGHT=224"
+set_global_assignment -name VERILOG_MACRO "JTFRAME_SDRAM_REPACK=1"
