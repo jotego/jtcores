c4517e935c56b27045b3ae10ce78ab82ce71d675
Jose Tejada <jotego@gmail.com>
2020-04-15 09:26:39 +0200

    Updated Unamiga/Cyclone5

--- cores/commando/cyclone5/jtcommando.tcl
+++ cores/commando/cyclone5/jtcommando.tcl
@@ -1,10 +1,12 @@
-set_global_assignment -name VERILOG_MACRO "CORENAME=\"JT1943\""
-set_global_assignment -name VERILOG_MACRO "VERTICAL_SCREEN=1"
-set_global_assignment -name VERILOG_MACRO "JTFRAME_OSD_TEST=1"
-set_global_assignment -name VERILOG_MACRO "GAMETOP=jt1943_game"
-set_global_assignment -name VERILOG_MACRO "ZX2TOP=jt1943_mist"
-set_global_assignment -name VERILOG_MACRO "JT12=1"
+set_global_assignment -name VERILOG_MACRO "CORETOP=jtcommando_cyclone5"
 
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
-set_global_assignment -name VERILOG_FILE ../../modules/jt12/hdl/mixer/jt12_mixer.v
-set_global_assignment -name VERILOG_MACRO "OSD_NOBCK=<None>"
+set_global_assignment -name VERILOG_MACRO "CORENAME=\"JTCOM\""
+set_global_assignment -name VERILOG_MACRO "VERTICAL_SCREEN=1"
+set_global_assignment -name VERILOG_MACRO "HAS_TESTMODE=1"
+set_global_assignment -name VERILOG_MACRO "GAMETOP=jtcommando_game"
+set_global_assignment -name VERILOG_MACRO "JT12=1"
+set_global_assignment -name VERILOG_MACRO "VIDEO_WIDTH=384"
+
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/jt49/hdl/filter/jt49_dcrm2.v
+set_global_assignment -name VERILOG_FILE ../../../modules/jt12/hdl/mixer/jt12_mixer.v
+ 
