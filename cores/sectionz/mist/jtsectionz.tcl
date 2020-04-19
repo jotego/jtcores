07ce54091af56acd1c5b0d90dfdd2b9e5006d84a
Jose Tejada <jotego@gmail.com>
2020-04-19 20:06:18 +0200

    Preparing Section Z release

--- cores/sectionz/mist/jtsectionz.tcl
+++ cores/sectionz/mist/jtsectionz.tcl
@@ -1,6 +1,5 @@
 set_global_assignment -name VERILOG_MACRO "MISTTOP=jtsectionz_mist"
-# JTGNG_VGA scan doubler
-set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=1"
-set_global_assignment -name VERILOG_MACRO "JTFRAME_VGA=1"
+set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=2"
+set_global_assignment -name VERILOG_MACRO "MISTER_VIDEO_MIXER=1"
 
 set_global_assignment -name VERILOG_MACRO "JTFRAME_MIST_DIRECT=0"
