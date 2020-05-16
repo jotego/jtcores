62092e5be923ed78d316d87513df66e3dd742713
Jose Tejada <jotego@gmail.com>
2020-05-16 11:50:32 +0200

    Added github sponsor button

--- cores/gng/mist/jtgng.tcl
+++
@@ -1,5 +0,0 @@
-set_global_assignment -name VERILOG_MACRO "MISTTOP=jtgng_mist"
-
-# JTGNG_VGA scan doubler
-set_global_assignment -name VERILOG_MACRO "SCAN2X_TYPE=1"
-set_global_assignment -name VERILOG_MACRO "JTFRAME_VGA=1"
