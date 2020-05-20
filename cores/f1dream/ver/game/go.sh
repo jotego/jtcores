6edd00c5188fb91845db2492a7bbd73061d9be47
jotego <jotego@gmail.com>
2020-05-20 13:33:58 +0200

    Fixed missing clock speed in F1DREAM

--- cores/f1dream/ver/game/go.sh
+++ cores/f1dream/ver/game/go.sh
@@ -30,6 +30,6 @@ fi
 echo "Game ROM length: " $GAME_ROM_LEN
 ../../../modules/jtframe/bin/sim.sh $MIST -d GAME_ROM_LEN=$GAME_ROM_LEN \
     -sysname tora -modules ../../../modules -d SCANDOUBLER_DISABLE=1 \
-    -d JTFRAME_CLK24 -d JTFRAME_MRA_DIP -d BUTTONS=2 \
+    -def ../../hdl/jtf1dream.def \
     -d FAST_LOAD \
-    -d F1DREAM $*
+    $*
