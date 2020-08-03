87f7b7b1edc24d8753d967ee86a4e67be77d44b2
Jose Tejada <jotego@gmail.com>
2020-08-03 20:46:22 +0200

    BG1 colours ok

--- rom/mra/makemra.sh
+++ rom/mra/makemra.sh
@@ -5,6 +5,19 @@
 # gfx4 SCR2
 # gfx5 map ROM for SCR2
 
+MAKEROM=0
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -rom)
+            MAKEROM=1;;
+        *)
+            echo "ERROR: unknown argument " $MAKEROM
+            exit 1;;
+    esac
+    shift
+done
+
 export JTFRAME
 (cd $JTFRAME/cc;make) || exit $?
 
@@ -16,19 +29,22 @@ mame2dip trojan.xml \
     -start gfx1 0x30000 \
     -order maincpu soundcpu adpcm gfx5 gfx1 gfx2 gfx4 gfx3 \
     -order-roms maincpu 1 2 0 \
-    -order-roms gfx2 0 1 2 3 4 5 6 7 \
+    -order-roms gfx2  6 7 4 5 2 3 0 1\
     -header 32 -header-data 2 \
     -header-offset 8 soundcpu gfx1 gfx2 gfx3 proms
 
-for i in Tro*mra 'Tatakai no Banka (Japan).mra'; do
-    mra -z /opt/mame "$i"
-done
 
-# mkdir -p _alternatives/_Trojan
-# mv 'Trojan (bootleg).mra' 'Trojan (US set 1).mra' 'Trojan (US set 2).mra' 'Tatakai no Banka (Japan).mra' _alternatives/_Trojan
-#
-# if [ -d /media/jtejada/MIST ]; then
-#     cp *.rom /media/jtejada/MIST
-# fi
+if [ $MAKEROM = 1 ]; then
+    for i in Tro*mra 'Tatakai no Banka (Japan).mra'; do
+        mra -z /opt/mame "$i"
+    done
 
-mv *.rom ..
+    if [ -d /media/jtejada/MIST ]; then
+        cp *.rom /media/jtejada/MIST
+    fi
+    mv *.rom ..
+fi
+
+mkdir -p _alternatives/_Trojan
+mv 'Trojan (bootleg).mra' 'Trojan (US set 1).mra' 'Trojan (US set 2).mra' 'Tatakai no Banka (Japan).mra' _alternatives/_Trojan
+
