cdbe6d3cfd09633704568880268e586815df79fd
Jose Tejada <jotego@gmail.com>
2021-05-20 15:30:03 +0200

    SF MRA WIP

--- rom/makemra.sh
+++ rom/makemra.sh
@@ -25,6 +25,7 @@ makemra.sh creates MRA files for some cores. Optional arguments:
                 sarms
                 exed
                 hige
+                rumble
     -h | -help  shows this message
 EOF
             exit 1;;
@@ -36,26 +37,28 @@ EOF
 done
 
 export JTFRAME
-(cd $JTFRAME/cc;make) || exit $?
+OUTDIR=mra
 
-
+function set_alt {
+    ALTDIR=_alternatives/"_$1"
+    mkdir -p $OUTDIR/"$ALTDIR"
+}
 
 ################## Higemaru
 if [[ $MAKELIST = all || $MAKELIST == rumble ]]; then
-OUTDIR=.
-ALTDIR=_alternatives/"_The Speed Rumbler"
-mkdir -p $OUTDIR/"$ALTDIR"
+set_alt "The Speed Rumbler"
 mame2dip srumbler.xml -outdir $OUTDIR -altfolder "$ALTDIR" \
     -rbf jtrumble \
     -rename char=gfx1 -swapbytes char \
     -rename scr=gfx2 -order-roms scr 4 0 5 1 6 2 7 3 -frac 1 scr 2 \
     -rename obj=gfx3 -order-roms obj 6 4 2 0 7 5 3 1 -frac 1 obj 4 \
     -corebuttons 2 -buttons "Shoot,Exit car"
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 ################## Higemaru
 if [[ $MAKELIST = all || $MAKELIST == hige ]]; then
-mame2dip higemaru.xml \
+mame2dip higemaru.xml -outdir $OUTDIR\
     -rbf jthige \
     -rename char=gfx1 obj=gfx2 \
     -frac obj 2 \
@@ -63,40 +66,44 @@ mame2dip higemaru.xml \
     -buttons action unused \
     -order-roms obj 1 0 \
     -rmdipsw Unused -4way
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 ################## Street Fighter 1
 if [[ $MAKELIST = all || $MAKELIST == sf ]]; then
-mame2dip sf.xml \
+set_alt "Street Fighter"
+
+mame2dip sf.xml -outdir $OUTDIR -altfolder "$ALTDIR"\
     -rbf jtsf \
     -rename scr2=gfx1 scr1=gfx2 obj=gfx3 char=gfx4 maps=tilerom mcu=protcpu? \
     -swapbytes audiocpu audio2 \
-    -frac obj 2 \
+    -setword maincpu 16 \
+    -frac obj  2 \
     -frac scr1 2 \
     -frac scr2 2 \
     -frac maps 2 \
-    -frac maincpu 2 \
-    -order-roms maincpu 0 2 4 1 3 5 \
-    -order-roms maps 3 1 2 0 \
-    -order-roms scr1 4 5 6 7 0 1 2 3 \
-    -order-roms obj 7 8 9 10 11 12 13 0 1 2 3 4 5 6 \
-    -order-roms scr2 2 3 0 1 \
+    -order-roms maps 3 2 1 0 \
+    -order-roms scr1 4 0 5 1 6 2 7 3 \
+    -order-roms scr2 2 0 3 1 \
+    -order-roms obj 7 0 8 1 9 2 10 3 11 4 12 5 13 6 \
     -order maincpu audiocpu audio2 mcu maps char scr1 scr2 obj prom \
     -start maps 0xa9000 \
-    -buttons punch1 punch2 punch3 kick1 kick2 kick3 \
+    -buttons "punch1,punch2,punch3,kick1,kick2,kick3" \
     -dipbase 8 -rmdipsw Unused -rmdipsw Freeze
 
 # Fix DIP names
-find -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Number of Countries Selected/Countries/g"
-find -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Stage Maximum/Stage Max/g"
-find -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Round Time Count/Time/g"
+find $OUTDIR -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Number of Countries Selected/Countries/g"
+find $OUTDIR -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Stage Maximum/Stage Max/g"
+find $OUTDIR -name "Street Fighter*.mra" -print0 | xargs -0 sed -i "s/Round Time Count/Time/g"
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 ################## Side Arms
 # gfx2 = 32x32 tiles
 # gfx3 = OBJ
 if [[ $MAKELIST = all || $MAKELIST == sarms ]]; then
-mame2dip sidearms.xml \
+set_alt "Side Arms"
+mame2dip sidearms.xml -outdir $OUTDIR -altfolder "$ALTDIR"\
     -rbf jtsarms \
     -frac gfx2 2 \
     -frac gfx3 2 \
@@ -105,21 +112,25 @@ mame2dip sidearms.xml \
     -rmdipsw Freeze \
     -buttons "fire-left" "fire-right" "option"
 #    -order-roms gfx2  4 5 6 7 0 1 2 3
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 ################## Exed Exes
 # gfx3 = 16x16 tiles
 # gfx4 = OBJ
 if [[ $MAKELIST = all || $MAKELIST == exed ]]; then
-mame2dip exedexes.xml \
+set_alt "Exed Exes"
+mame2dip exedexes.xml -outdir $OUTDIR -altfolder "$ALTDIR"\
     -rbf jtexed \
     -frac gfx3 2 \
     -frac gfx4 2
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 ############# Trojan
 if [[ $MAKELIST = all || $MAKELIST == trojan ]]; then
-mame2dip trojan.xml \
+set_alt "Trojan"
+mame2dip trojan.xml -outdir $OUTDIR -altfolder "$ALTDIR"\
     -rbf jttrojan \
     -frac gfx2 4 \
     -frac gfx3 2 \
@@ -131,6 +142,7 @@ mame2dip trojan.xml \
     -order-roms gfx2  6 7 4 5 2 3 0 1\
     -header 32 -header-data 2 \
     -header-offset 8 soundcpu gfx1 gfx2 gfx3 proms
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
 if [ $MAKEROM = 1 ]; then
@@ -150,19 +162,6 @@ if [ $MAKEROM = 1 ]; then
         cp *.rom /media/jtejada/MIST
     fi
     mv *.rom ..
+if [[ $MAKELIST != all ]]; then exit 0; fi
 fi
 
-mkdir -p _alternatives/_Street\ Fighter
-mkdir -p _alternatives/_Trojan
-mkdir -p _alternatives/_Side\ Arms
-mv 'Trojan (bootleg).mra' 'Trojan (US set 1).mra' 'Trojan (US set 2).mra' 'Tatakai no Banka (Japan).mra' _alternatives/_Trojan 2> /dev/null
-mv Side*US*.mra Side*Japan*.mra _alternatives/_Side\ Arms  2> /dev/null
-for i in  'Street Fighter (Japan, bootleg).mra' \
-          'Street Fighter (Japan, pneumatic buttons).mra' \
-          'Street Fighter (US, set 2) (protected).mra' \
-          'Street Fighter (Japan) (protected).mra' 'Street Fighter (World, pneumatic buttons).mra' \
-          'Street Fighter (prototype).mra' 'Street Fighter (World) (protected).mra'; do
- if [ -e "$i" ]; then
-    mv "$i" _alternatives/_Street\ Fighter
-fi
-done
