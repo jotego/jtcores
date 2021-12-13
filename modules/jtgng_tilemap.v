ef2483b9c4ddb4d51e84a11a172fa0dd389bb853
Jose Tejada <jotego@gmail.com>
2021-12-13 19:55:24 +0100

    Flip mode fixed in Side Arms / improved in SF1

--- modules/jtgng_tilemap.v
+++ modules/jtgng_tilemap.v
@@ -81,7 +81,7 @@ always @(*) begin
                 { V[8:7], H[9:7], V[6:3], H[6:3] } :
                 { V[9:7], H[8:7], V[6:3], H[6:3] };
         end else if( LAYOUT==8 || LAYOUT==9 ) begin // Side Arms/Street Fighter
-            scan = { V[7:3], H[8:3] }; // SCANW assumed to be 11
+            scan = {SCANW{flip}}^{ V[7:3], H[8:3] }; // SCANW assumed to be 11
         end else if( LAYOUT==10 ) begin // The Speed Rumbler (SCANW=11 or 12)
             scan = {SCANW{flip}}^{ H[8:3], V[7:(SCANW==12?2:3)] };
         end else // other games
