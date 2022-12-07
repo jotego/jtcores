1b6d11a17ec94b65e1c714401bb7cd18ff6de783
Jose Tejada <jotego@gmail.com>
2022-12-07 17:44:46 +0100

    JTFRAME: Pocket sound on Dock

---
+++ rom/cp_gng.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp gng.rom /media/$USER/POCKET/Assets/jtgng/common
+cp gnga.rom /media/$USER/POCKET/Assets/jtgng/common
+cp gngc.rom /media/$USER/POCKET/Assets/jtgng/common
+cp gngprot.rom /media/$USER/POCKET/Assets/jtgng/common
+cp gngt.rom /media/$USER/POCKET/Assets/jtgng/common
+cp makaimur.rom /media/$USER/POCKET/Assets/jtgng/common
+cp makaiurc.rom /media/$USER/POCKET/Assets/jtgng/common
+cp makaiurg.rom /media/$USER/POCKET/Assets/jtgng/common
