d434925266fb4797eb9d01e375b90db2de04adc7
Jose Tejada <jotego@gmail.com>
2023-04-19 08:35:26 +0200

    ngp: adding main/sound modules - wip

---
+++ cores/ngp/hdl/jtngp_snd.v
@@ -0,0 +1,49 @@
+/*  This file is part of JTCORES.
+    JTCORES program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JTCORES program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JTCORES.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Jose Tejada Gomez. Twitter: @topapate
+    Version: 1.0
+    Date: 19-3-2023 */
+
+module jtngp_snd(
+    input             rst,
+    input             clk,
+);
+
+jtframe_z80_romwait u_cpu(
+    .rst_n      ( ~rst      ),
+    .clk        ( clk       ),
+    .cen        ( cen_fm    ),
+    .cpu_cen    ( cpu_cen   ),
+    .int_n      ( int_n     ),
+    .nmi_n      ( nmi_n     ),
+    .busrq_n    ( 1'b1      ),
+    .m1_n       ( m1_n      ),
+    .mreq_n     ( mreq_n    ),
+    .iorq_n     ( iorq_n    ),
+    .rd_n       ( rd_n      ),
+    .wr_n       ( wr_n      ),
+    .rfsh_n     ( rfsh_n    ),
+    .halt_n     (           ),
+    .busak_n    (           ),
+    .A          ( A         ),
+    .cpu_din    ( cpu_din   ),
+    .cpu_dout   ( cpu_dout  ),
+    .ram_dout   ( ram_dout  ),
+    // ROM access
+    .rom_cs     ( rom_cs    ),
+    .rom_ok     ( rom_ok    )
+);
+
+endmodule
