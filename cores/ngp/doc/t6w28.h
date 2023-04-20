6ee0db2ae5622d52d9f9d8ce7ed493b227ff2d0a
Jose Tejada <jotego@gmail.com>
2023-04-20 08:53:37 +0200

    ngp: added sound module

---
+++ cores/ngp/doc/t6w28.h
@@ -0,0 +1,45 @@
+// license:BSD-3-Clause
+// copyright-holders:Wilbert Pol
+#ifndef MAME_SOUND_T6W28_H
+#define MAME_SOUND_T6W28_H
+
+#pragma once
+
+class t6w28_device : public device_t, public device_sound_interface
+{
+public:
+	t6w28_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+	void write(offs_t offset, uint8_t data);
+	void set_enable(bool enable);
+
+protected:
+	// device-level overrides
+	virtual void device_start() override;
+
+	// sound stream update overrides
+	virtual void sound_stream_update(sound_stream &stream, std::vector<read_stream_view> const &inputs, std::vector<write_stream_view> &outputs) override;
+
+	void set_gain(int gain);
+
+private:
+	sound_stream *m_channel;
+	int m_sample_rate;
+	int m_vol_table[16];    /* volume table         */
+	int32_t m_register[16];   /* registers */
+	int32_t m_last_register[2];   /* last register written */
+	int32_t m_volume[8];  /* volume of voice 0-2 and noise */
+	uint32_t m_rng[2];        /* noise generator      */
+	int32_t m_noise_mode[2];  /* active noise mode */
+	int32_t m_feedback_mask;     /* mask for feedback */
+	int32_t m_whitenoise_taps;   /* mask for white noise taps */
+	int32_t m_whitenoise_invert; /* white noise invert flag */
+	int32_t m_period[8];
+	int32_t m_count[8];
+	int32_t m_output[8];
+	bool m_enabled;
+};
+
+DECLARE_DEVICE_TYPE(T6W28, t6w28_device)
+
+#endif // MAME_SOUND_T6W28_H
--- cores/ngp/hdl/jtngp_snd.v
+++ cores/ngp/hdl/jtngp_snd.v
@@ -17,33 +17,111 @@
     Date: 19-3-2023 */
 
 module jtngp_snd(
-    input             rst,
-    input             clk,
+    input                rst,
+    input                clk,
+
+    input         [11:0] main_addr,
+    input         [15:0] main_dout,
+    input         [15:0] main_din,
+    input         [ 1:0] main_we,
+    output               main_irqn,
+    input         [ 7:0] comm,      // where do we store these 8 bits?
+    input                int_n,
+
+    output               sample,
+    output signed [11:0] snd
 );
 
-jtframe_z80_romwait u_cpu(
+wire [15:0] cpu_addr;
+wire [ 1:0] ram_bwe;
+reg  [ 7:0] cpu_din;
+wire [ 7:0] ram_lsb, ram_msb;
+reg         ram_cs, psg_cs, latch_cs, intset_cs;
+wire        wr_n, mreq_n, cen_snd;
+
+assign sample  = 0;
+assign snd     = 0;
+assign ram_bwe = {2{ram_cs&~wr_n}} & { cpu_addr[0], ~cpu_addr[0] };
+
+jtframe_cen3p57 u_cen(
+    .clk        ( clk       ),
+    .cen_3p57   ( cen_snd   ),
+    .cen_1p78   (           )
+);
+
+always @* begin
+    ram_cs    = !mreq_n && cpu_addr[15:14]==0;
+    psg_cs    = !mreq_n && cpu_addr[15:14]==1;
+    latch_cs  = !mreq_n && cpu_addr[15:14]==2; // part of main's IO map
+    intset_cs = !mreq_n && cpu_addr[15:14]==3;
+end
+
+always @(posedge clk) begin
+    cpu_din <=  latch_cs ? comm :
+                ram_cs   ? (cpu_addr[0] ? ram_msb : ram_lsb );
+end
+
+jtframe_edge #(.QSET(0)) u_mainint(
+    .rst    ( rst       ),
+    .clk    ( clk       ),
+    .edgeof ( intset_cs ),
+    .clr    ( ~iorq_n   ),
+    .q      ( main_irqn )
+);
+
+jtframe_dual_ram #(.AW(11)) u_ramlow(
+    // Port 0
+    .clk0   ( clk             ),
+    .data0  ( main_dout[7:0]  ),
+    .addr0  ( main_addr[11:1] ),
+    .we0    ( main_we[0]      ),
+    .q0     ( main_din[7:0]   ),
+    // Port 1
+    .clk1   ( clk             ),
+    .data1  ( cpu_dout        ),
+    .addr1  ( cpu_addr[11:1]  ),
+    .we1    ( ram_bwe[0]      ),
+    .q1     ( ram_lsb         )
+);
+
+jtframe_dual_ram #(.AW(11)) u_ramhi(
+    // Port 0
+    .clk0   ( clk             ),
+    .data0  ( main_dout[15:8] ),
+    .addr0  ( main_addr[11:1] ),
+    .we0    ( main_we[1]      ),
+    .q0     ( main_din[15:8]  ),
+    // Port 1
+    .clk1   ( clk             ),
+    .data1  ( cpu_dout        ),
+    .addr1  ( cpu_addr[11:1]  ),
+    .we1    ( ram_bwe[1]      ),
+    .q1     ( ram_msb         )
+);
+
+jtframe_z80_romwait #(.CLR_INT(1)) u_cpu(
     .rst_n      ( ~rst      ),
     .clk        ( clk       ),
-    .cen        ( cen_fm    ),
-    .cpu_cen    ( cpu_cen   ),
+    .cen        ( cen_snd   ),
+    .cpu_cen    (           ),
     .int_n      ( int_n     ),
-    .nmi_n      ( nmi_n     ),
+    .nmi_n      ( 1'b1      ),
     .busrq_n    ( 1'b1      ),
-    .m1_n       ( m1_n      ),
+    .m1_n       (           ),
     .mreq_n     ( mreq_n    ),
     .iorq_n     ( iorq_n    ),
-    .rd_n       ( rd_n      ),
+    .rd_n       (           ),
     .wr_n       ( wr_n      ),
-    .rfsh_n     ( rfsh_n    ),
+    .rfsh_n     (           ),
     .halt_n     (           ),
     .busak_n    (           ),
-    .A          ( A         ),
+    .A          ( cpu_addr  ),
     .cpu_din    ( cpu_din   ),
     .cpu_dout   ( cpu_dout  ),
     .ram_dout   ( ram_dout  ),
     // ROM access
-    .rom_cs     ( rom_cs    ),
-    .rom_ok     ( rom_ok    )
+    .rom_cs     ( 1'b0      ),
+    .rom_ok     ( 1'b1      )
 );
 
 endmodule
