a9496eb209000e1c892c7d0674ad51f45dbaeb54
Jose Tejada <jotego@gmail.com>
2023-09-15 10:08:39 +0200

    BETAKEY as a GitHub secret. Fixed JTTMNT beta on the Pocket

--- modules/jtframe/devops/xjtcore.sh
+++ modules/jtframe/devops/xjtcore.sh
@@ -6,7 +6,12 @@ export JTFRAME=$JTROOT/modules/jtframe
 
 source $JTFRAME/bin/setprj.sh
 export PATH=$PATH:/usr/local/go/bin
+
+if [ ! -z "$2" ]; then
+    BETAKEY="-d JTFRAME_UNLOCKKEY=$2"
+fi
+
 if [ -e $CORES/$1/cfg/macros.def ]; then
     jtframe
-    jtseed 3 $1 -mister --nodbg
+    jtseed 3 $1 -mister --nodbg $BETAKEY
 fi
