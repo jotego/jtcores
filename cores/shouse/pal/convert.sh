eac25cde7e47c2aba6905ac6628610614e9f6267
Jose Tejada <jotego@gmail.com>
2023-09-21 09:04:01 +0200

    shouse: PALs

---
+++ cores/shouse/pal/convert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# PALs from https://wiki.pldarchive.co.uk/index.php?title=Splatter_House
+jedutil -view Namco-SYS87A-C.jed GAL16V8 > L2.txt
+jedutil -view Namco-SYS87A-R.jed GAL16V8 > K3.txt
