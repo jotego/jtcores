jotego/jtcores

commit 508f763ecd727cbbf03bb2557fcd091d2be0e223

shouse: CUS30 sound ok. FM sound too slow

modules/jtframe/hdl/clocking/jtframe_gated_cen.v
@@ -23,7 +23,7 @@
 // generation is halted while busy input is high, and lost
 // cycles are recovered once busy goes low
 
-/* verilator tracing_off */
+/* verilator tracing_on */
 
 module jtframe_gated_cen #( parameter
     W     =  2,

cores/shouse/doc/namco_snd.cpp
@@ -758,7 +758,7 @@ void namco_audio_device::sound_stream_update(sound_stream &stream, std::vector<r
 
 				/* only update if we have non-zero left volume */
 				if (lv)
-				{
+				{	// MAME has the sound tables pre-calculated with the different gains applied
 					const int16_t *lw = &m_waveform[lv][voice->waveform_select * 32];
 
 					/* generate sound into the buffer */

cores/shouse/hdl/jtcus30.sv
@@ -29,7 +29,7 @@ module jtcus30(
     input               rst,
     input               clk,
     input               bsel,
-    input               cen,    // 1.5 MHz
+    input               cen,    // 1.5 MHz (16 times slower than original input clk at 24MHz)
 
     output       [ 7:0] xdin,
     // main/sub bus
@@ -45,8 +45,11 @@ module jtcus30(
     input        [ 7:0] sdout,
 
     // sound output
-    output reg   [10:0] snd_l,
-    output reg   [10:0] snd_r
+    output                   sample,
+    output reg signed [12:0] snd_l,
+    output reg signed [12:0] snd_r,
+
+    input             [ 7:0] debug_bus
 );
 
 localparam CW=21,
@@ -70,15 +73,37 @@ reg  [7:0][CW-1:0] cnt;
 
 // sound synthesis
 reg  [ 2:0] ch, ch_l;
-reg  [10:0] lacc, racc;
-wire [ 7:0] wdata;
-reg  [ 7:0] lamp, ramp;
-wire [ 9:0] rd_addr = {1'b0, wsel[ch], cnt[ch][A0+:5] };
+reg  [12:0] lacc, racc, raw_l, raw_r;
+wire [ 7:0] wdata8;
+reg  signed [ 9:0] lamp, ramp;
+wire [ 9:0] rd_addr = {2'b0, wsel[ch], cnt[ch][(A0+1)+:4] };
+wire signed [ 3:0] wdata;
 
 // LFSR polynomial
 reg  [17:0] lfsr;
 reg         noise;
 reg  [7:0][CW-1:A0] cnt_no; // old value of counter when noise was last produced
+
+assign sample = ch==0 && cen120;
+assign wdata  = (cnt[ch][A0] ? wdata8[3:0] : wdata8[7:4])^{ debug_bus[1],3'd0};
+
+// `ifdef SIMULATION
+// reg [7:0][3:0] wav;
+// wire [ 3:0] wav0,  wav1,  wav2,  wav3,  wav4,  wav5,  wav6,  wav7;
+// wire [19:0] freq0, freq1, freq2, freq3, freq4, freq5, freq6, freq7;
+// wire [ 3:0] lvol0, lvol1, lvol2, lvol3, lvol4, lvol5, lvol6, lvol7;
+// wire [CW-1:0] wcnt0, wcnt1, wcnt2, wcnt3, wcnt4, wcnt5, wcnt6, wcnt7;
+
+// assign  freq0 = freq[0], freq1 = freq[1], freq2 = freq[2], freq3 = freq[3],
+//         freq4 = freq[4], freq5 = freq[5], freq6 = freq[6], freq7 = freq[7];
+// assign  wav0 = wav[0], wav1 = wav[1], wav2 = wav[2], wav3 = wav[3],
+//         wav4 = wav[4], wav5 = wav[5], wav6 = wav[6], wav7 = wav[7];
+// assign  lvol0 = lvol[0], lvol1 = lvol[1], lvol2 = lvol[2], lvol3 = lvol[3],
+//         lvol4 = lvol[4], lvol5 = lvol[5], lvol6 = lvol[6], lvol7 = lvol[7];
+// assign  wcnt0 = cnt[0], wcnt1 = cnt[1], wcnt2 = cnt[2], wcnt3 = cnt[3],
+//         wcnt4 = cnt[4], wcnt5 = cnt[5], wcnt6 = cnt[6], wcnt7 = cnt[7];
+// always @(posedge clk) if(cen120) wav[ch]<=wdata;
+// `endif
 
 always @(posedge clk, posedge rst ) begin
     if( rst ) begin
@@ -102,13 +127,13 @@ always @(posedge clk, posedge rst ) begin
             ramp <= 4'd7*({1'b0,rvol[ch_l][3:1]});
             cnt_no[ch_l] <= cnt[ch_l][CW-1:A0];
         end else begin
-            lamp <= wdata*lvol[ch_l];
-            ramp <= wdata*rvol[ch_l];
+            lamp <= { wdata[3] & debug_bus[0], wdata } * { 1'b0, lvol[ch_l] };
+            ramp <= { wdata[3] & debug_bus[0], wdata } * { 1'b0, rvol[ch_l] };
         end
 
         // accumulator and output
-        lacc <= ch==0 ? {3'd0,lamp} : lacc+{3'd0,lamp};
-        racc <= ch==0 ? {3'd0,ramp} : racc+{3'd0,ramp};
+        lacc <= ch==0 ? {{3{lamp[9]}},lamp} : lacc+{ {3{lamp[9]}},lamp};
+        racc <= ch==0 ? {{3{lamp[9]}},ramp} : racc+{ {3{lamp[9]}},ramp};
         if( ch==0 ) begin
             snd_l <= lacc;
             snd_r <= racc;
@@ -119,7 +144,7 @@ always @(posedge clk, posedge rst ) begin
     end
 end
 
-jtframe_cendiv #(.MDIV(12)) u_cendiv(
+jtframe_cendiv #(.MDIV(8)) u_cendiv(
     .clk        ( clk       ),
     .cen_in     ( cen       ),
     .cen_div    ( cen120    ),
@@ -138,7 +163,7 @@ jtframe_dual_ram u_wave( // 4 (waves) + 5 (wave length) = 9 bits, 10th bit must 
     .data1  (  8'd0      ),
     .addr1  ( rd_addr    ),
     .we1    (  1'b0      ),
-    .q1     ( wdata      )
+    .q1     ( wdata8     )
 );
 
 jtshouse_cus30_mmr u_mmr(

cores/shouse/hdl/jtshouse_cenloop.v
@@ -16,7 +16,7 @@
     Version: 1.0
     Date: 22-9-2023 */
 
-/* verilator tracing_on */
+/* verilator tracing_off */
 module jtshouse_cenloop(
     input             rst,
     input             clk,

cores/shouse/hdl/jtshouse_game.v
@@ -228,7 +228,8 @@ jtshouse_sound u_sound(
     .left       ( snd_left  ),
     .right      ( snd_right ),
     .sample     ( sample    ),
-    .peak       ( game_led  )
+    .peak       ( game_led  ),
+    .debug_bus  ( debug_bus )
 );
 
 jtshouse_triram u_triram(

cores/shouse/hdl/jtshouse_key.v
@@ -18,7 +18,7 @@
 
 // The implementation of the KEY chips follows MAME's namcos1_m.cpp
 // These chips won't impact any timing accuracy
-
+/* verilator tracing_off */
 module jtshouse_key(
     input               rst,
     input               clk,

cores/shouse/hdl/jtshouse_mcu.v
@@ -137,6 +137,7 @@ jtframe_dcrm #(.SW(11)) u_dcrm(
     .din        ( mix       ),
     .dout       ( snd       )
 );
+
 always @(posedge clk, negedge rstn ) begin
     if( !rstn ) begin
         bank     <= 0;
@@ -210,8 +211,7 @@ jt63701v #(.ROMW(12),.SLOW_FRC(2)) u_63701(
     .rom_cs     (               ),
     .rom_addr   ( rom_addr      ),
     .rom_data   ( rom_data      ),
-    .irq_ack    ( irq_ack       ),
-    .debug_bus  ( debug_bus     )
+    .irq_ack    ( irq_ack       )
 );
 
 jtframe_prom #(.AW(12)) u_prom(

cores/shouse/hdl/jtshouse_sound.v
@@ -50,7 +50,8 @@ module jtshouse_sound(
     input  signed[10:0] pcm_snd,
     output signed[15:0] left, right,
     output              sample,
-    output              peak
+    output              peak,
+    input        [ 7:0] debug_bus
 );
 `ifndef NOSOUND
 localparam [7:0] FMGAIN =8'h10,
@@ -60,7 +61,7 @@ localparam [7:0] FMGAIN =8'h10,
 wire [15:0] A;
 wire [ 7:0] fm_dout;
 wire [11:0] snd_l, snd_r;
-wire [10:0] cus30_l, cus30_r;
+wire [12:0] cus30_l, cus30_r;
 reg  [ 7:0] cpu_din;
 reg  [ 2:0] bank;
 reg         irq_n, lvbl_l, VMA, rst, bsel;
@@ -149,8 +150,11 @@ jtcus30 u_wav(
 
     // sound output
     .snd_l  ( cus30_l   ),
-    .snd_r  ( cus30_r   )
+    .snd_r  ( cus30_r   ),
+    .sample (           ),
+    .debug_bus(debug_bus)
 );
+
 /* verilator tracing_off */
 jt51 u_jt51(
     .rst        ( ~srst_n   ), // reset
@@ -174,7 +178,7 @@ jt51 u_jt51(
     .xright     ( fm_r      )
 );
 
-jtframe_mixer #(.W1(11),.W2(11)) u_right(
+jtframe_mixer #(.W1(11),.W2(13)) u_right(
     .rst    ( rst       ),
     .clk    ( clk       ),
     .cen    ( 1'b1      ),
@@ -192,7 +196,7 @@ jtframe_mixer #(.W1(11),.W2(11)) u_right(
     .peak   ( peak_r    )
 );
 
-jtframe_mixer #(.W1(11),.W2(11)) u_left(
+jtframe_mixer #(.W1(11),.W2(13)) u_left(
     .rst    ( rst       ),
     .clk    ( clk       ),
     .cen    ( 1'b1      ),

commit cc677db0452b9ab0f1d6bcbce0bcfb82b691e730

Fixes TMNT title music broken in 508f763ecd727cbbf03bb2557fcd091d2be0e223

modules/jtframe/hdl/clocking/jtframe_gated_cen.v
@@ -60,7 +60,7 @@ always @(posedge clk) begin
         toggle <= toggle + 1'd1;
         cen[0] <= 1;
         for( i=1; i<W; i=i+1 ) begin
-            cen[i] <= toggle[i-1];
+            cen[i] <= (toggle&((1<<i)-1))==(1<<i)-1;
         end
     end else begin
         cen <= 0;