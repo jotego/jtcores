98f5a87e46b07777cb31eb2e45eecf9b457f2b2c
Jose Tejada <jose.tejada@jotego.es>
2023-11-05 08:00:58 +0100

    #283 wip

--- modules/jtframe/devops/docker-jtcore.sh
+++ modules/jtframe/devops/docker-jtcore.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 --shallow-submodules  --recurse-submodules git@github.com:jotego/jtcores.git
+git clone --shallow-submodules  --recurse-submodules git/jtcores.git
 cd jtcores
 COMMIT=$1
 git checkout $COMMIT
 shift
 
-. setprj.sh
+. modules/jtframe/bin/setprj.sh
 jtframe
 cd $JTROOT
 
