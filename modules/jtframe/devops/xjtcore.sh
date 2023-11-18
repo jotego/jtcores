741793792dc04626c979e48af1157b97a00fa439
Jose Tejada <jose.tejada@jotego.es>
2023-11-18 07:04:24 +0100

    jtbin2sd: better handling of copying when rbf files are missing xjtcore.sh: only add the beta key to cores in beta phase

--- modules/jtframe/devops/xjtcore.sh
+++ modules/jtframe/devops/xjtcore.sh
@@ -7,15 +7,20 @@ export JTFRAME=$JTROOT/modules/jtframe
 source $JTFRAME/bin/setprj.sh
 export PATH=$PATH:/usr/local/go/bin
 
+CORENAME=$1
+
 BETAKEY="$2"
 if [ -z "$BETAKEY" ]; then
-    BETAKEY=`printf "0x%04X%04X" $RANDOM $RANDOM`
+    BETAKEY=`printf "%04X%04X" $RANDOM $RANDOM`
     echo "WARNING: remote compilation with no beta key. Assigning random one"
 fi
-BETAKEY="-d JTFRAME_UNLOCKKEY=$BETAKEY"
 
-if [ -e $CORES/$1/cfg/macros.def ]; then
-    jtframe
-    # Remote builds have red OSD and beta key enabled
-    jtseed 3 $1 -mister --nodbg $BETAKEY -d JTFRAME_OSDCOLOR=0x30
+export JTUTIL=/jtutil
+mkdir $JTUTIL
+printf "%08x" 0x$BETAKEY | xxd -r -p > $JTUTIL/beta.bin
+
+if [ -e $CORES/$CORENAME/cfg/macros.def ]; then
+    jtframe mra --skipROM $CORENAME
+    # Remote builds have red OSD. beta key is enabled for cores listed in beta.yaml
+    jtseed 3 $CORENAME -mister --nodbg -d JTFRAME_OSDCOLOR=0x30
 fi
