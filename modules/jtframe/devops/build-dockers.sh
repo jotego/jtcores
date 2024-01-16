63adad31a8c4b040493a75fa9111948ebfa8fb0a
Jose Tejada <jotego@gmail.com>
2024-01-16 19:35:16 +0100

    workflow using Quartus 20

--- modules/jtframe/devops/build-dockers.sh
+++ modules/jtframe/devops/build-dockers.sh
@@ -2,7 +2,9 @@
 docker image build --file jtcore-base.df --tag jotego/jtcore-base .
 docker image build --file jtcore13.df --tag jotego/jtcore13 /opt/altera
 docker image build --file jtcore17.df --tag jotego/jtcore17 /opt/intelFPGA_lite
+docker image build --file jtcore20.df --tag jotego/jtcore20 /opt/intelFPGA_lite .
 docker image build --file jtcore17x.df --tag jotego/jtcore17x .
+docker image build --file jtcore20x.df --tag jotego/jtcore20x .
 docker image build --file linter.df --tag jotego/linter .
 
 # Push the images to dockerhub
@@ -11,4 +13,6 @@ docker push jotego/jtcore-base:latest
 docker push jotego/jtcore13:latest
 docker push jotego/jtcore17:latest
 docker push jotego/jtcore17x:latest
+docker push jotego/jtcore20:latest
+docker push jotego/jtcore20x:latest
 docker push jotego/linter:latest
