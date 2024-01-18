63688ce5f4de9b92ac4d2ea4b306009b8ba4bcdb
Jose Tejada <jose.tejada@jotego.es>
2024-01-18 09:34:44 +0100

    correctly uncompress to the temporal release folder

--- modules/jtframe/bin/jtrelease.sh
+++ modules/jtframe/bin/jtrelease.sh
@@ -26,26 +26,31 @@ cd $DST
 git checkout $HASH
 git submodule init $JTFRAME/target/pocket
 git submodule update $JTFRAME/target/pocket
-jtframe
-unzip $REF
+jtframe > /dev/null
+unzip $REF -d release
 jtframe mra `find release/{mister,sidi} -name "*rbf*" | xargs -l -I_ basename _ .rbf | sort | uniq | sed s/^jt//`
 
 if [ -d /media/$USER/POCKET ]; then
 	jtbin2sd &
 fi
 
-if ping -c 1 -q $MRHOST > /dev/null; then
-	jtbin2mr
+if [ -n "$MRHOST" ]; then
+	if ping -c 1 -q $MRHOST > /dev/null; then
+		echo "Copying to MiSTer"
+		jtbin2mr;
+	fi
+else
+	echo "Define \$MRHOST to copy to MiSTer"
 fi
 
-if [[ -n "$JTBIN" && -d "$JTBIN" ]]; then
+if [[ -n "$JTBIN" && -d "$JTBIN" && "$JTBIN" != "$DST/release" ]]; then
+	echo "Copying to $JTBIN"
 	cd $JTBIN
 	cp -r $DST/release/* .
 else
 	echo "Skipping JTBIN as \$JTBIN is not defined"
 	exit 0
 fi
-
 
 wait
 rm -rf $DST
