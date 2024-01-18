7cfb88b9ee5ceed2870f9c192f4b2f3ffc1ab068
Jose Tejada <jose.tejada@jotego.es>
2024-01-18 19:20:52 +0100

    command line options

--- modules/jtframe/bin/jtrelease.sh
+++ modules/jtframe/bin/jtrelease.sh
@@ -3,8 +3,40 @@
 
 set -e
 
-HASH="$1"
-shift
+HASH=
+SKIPROM=
+VERBOSE=
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-l|--local)
+			unset JTBIN;;
+		--host)
+			shift
+			export MRHOST=$1;;
+		-s|--skipROM)
+			SKIPROM=--skipROM;;
+		-v|--verbose)
+			VERBOSE=1;;
+		-h|--help)
+			cat<<EOF
+
+	jtrelease.sh <hash> [arguments]
+
+	Copies a build from /nobackup/core-builds to the SD card,
+	MiSTer and JTBIN ($JTBIN)
+
+EOF
+			exit 0;;
+		*) if [[ -z "$HASH" && ${1:0:1} != - ]]; then
+			HASH=$1
+		else
+			echo "Do not know what to do with arguments $HASH and $1"
+			exit 1
+		fi
+	esac
+	shift
+done
 
 if [ -z "$HASH" ]; then
 	echo "Use jtrelease.sh git-hash"
@@ -28,20 +60,13 @@ git submodule init $JTFRAME/target/pocket
 git submodule update $JTFRAME/target/pocket
 jtframe > /dev/null
 unzip $REF -d release
-jtframe mra `find release/{mister,sidi} -name "*rbf*" | xargs -l -I_ basename _ .rbf | sort | uniq | sed s/^jt//`
+jtframe mra `find release/{mister,sidi} -name "*rbf*" | xargs -l -I_ basename _ .rbf | sort | uniq | sed s/^jt//` $SKIPROM
 
-if [ -d /media/$USER/POCKET ]; then
+if [ -z "$SKIPROM" ]; then
 	jtbin2sd &
 fi
 
-if [ -n "$MRHOST" ]; then
-	if ping -c 1 -q $MRHOST > /dev/null; then
-		echo "Copying to MiSTer"
-		jtbin2mr;
-	fi
-else
-	echo "Define \$MRHOST to copy to MiSTer"
-fi
+jtbin2mr
 
 if [[ -n "$JTBIN" && -d "$JTBIN" && "$JTBIN" != "$DST/release" ]]; then
 	echo "Copying to $JTBIN"
