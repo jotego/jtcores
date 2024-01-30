jotego/jtcores

commit 2a032d81e8729f4642ba864e99ad5b7b581d2b2b

sf obj restored. fixes #406

cores/sf/hdl/jtsf_game.v
@@ -119,25 +119,25 @@ localparam
     MCUW  = 12, // 4kB
     OBJW  = 21;
 
-localparam [21:0] MAIN_OFFSET = 22'h0,
-                  RAM_OFFSET  = 22'h4_0000,
+localparam [24:0] MAIN_OFFSET = 25'h0,
+                  RAM_OFFSET  = 25'h4_0000,
                   // Bank 1
-                  BA1_START   = 22'h6_0000,
-                  SND_OFFSET  = 22'h0,
-                  SND2_OFFSET = 22'h8000 >> 1,
-                  MCU_OFFSET  = 22'h4_8000 >> 1,
+                  BA1_START   = 25'h6_0000,
+                  SND_OFFSET  = 25'h0,
+                  SND2_OFFSET = 25'h8000 >> 1,
+                  MCU_OFFSET  = 25'h4_8000 >> 1,
                   // Bank 2
-                  BA2_START   = 22'hA_8000,
-                  MAP1_OFFSET = 22'h0,
-                  MAP2_OFFSET = 22'h2_0000 >> 1,
-                  CHAR_OFFSET = 22'h4_0000 >> 1,
+                  BA2_START   = 25'hA_8000,
+                  MAP1_OFFSET = 25'h0,
+                  MAP2_OFFSET = 25'h2_0000 >> 1,
+                  CHAR_OFFSET = 25'h4_0000 >> 1,
                   // Bank 3
-                  BA3_START   = 22'hE_C000,
-                  SCR1_OFFSET = 22'h0,
-                  SCR2_OFFSET = 22'h10_0000 >> 1,
-                  OBJ_OFFSET  = 22'h18_0000 >> 1;
-
-localparam [24:0] PROM_START  = 25'h42_C000;
+                  BA3_START   = 25'hE_C000,
+                  SCR1_OFFSET = 25'h0,
+                  SCR2_OFFSET = 25'h10_0000 >> 1,
+                  OBJ_START   = 25'h26_C000,
+                  OBJ_OFFSET  = 22'h18_0000 >> 1,
+                  PROM_START  = 25'h42_C000;
 
 wire [ 8:0] V;
 wire [ 8:0] H;
@@ -252,11 +252,12 @@ wire [12:0] obj_AB;
 wire [15:0] oram_dout;
 
 wire [21:0] pre_prog;
-wire        prom_we;
+wire        prom_we, prog_obj;
 reg         mcu_en, mcu_lock;
 
 // Optimize cache use for object ROMs
-assign prog_addr = (prog_ba == 2'd3 && prog_addr>=OBJ_OFFSET && ioctl_addr[22:1]<PROM_START[21:0]) ?
+assign prog_obj  = ioctl_addr>=OBJ_START;
+assign prog_addr = prog_obj ?
     { pre_prog[21:6],pre_prog[4:1],pre_prog[5],pre_prog[0]} :
     pre_prog;
 
@@ -428,8 +429,10 @@ jtsf_main #( .MAINW(MAINW), .RAMW(RAMW) ) u_main (
     assign RnW      = 1;
     assign UDSWn    = 1;
     assign LDSWn    = 1;
-    assign ram_addr = 14'd0;
+    assign ram_addr = 0;
     assign ram_cs   = 0;
+    assign col_uw   = 0;
+    assign col_lw   = 0;
     `endif
 `endif
 

commit e4c4af67b7eaa3ee8fcb4eefc57435abee6c181a

sf: Fixes #522. 8751 protected games work again after regression introduced in 2a032d8

cores/sf/hdl/jtsf_game.v
@@ -175,10 +175,9 @@ wire [15:0] oram_dout;
 
 wire [21:0] pre_prog;
 wire        prom_we, prog_obj;
-reg         mcu_en, mcu_lock;
 
 // Optimize cache use for object ROMs
-assign prog_obj  = ioctl_addr[24:0]>=OBJ_START;
+assign prog_obj  = ioctl_addr[24:0]>=OBJ_START && ioctl_addr[24:0]<PROM_START;
 assign prog_addr = prog_obj ?
     { pre_prog[21:6],pre_prog[4:1],pre_prog[5],pre_prog[0]} :
     pre_prog;
@@ -187,18 +186,6 @@ assign prog_addr = prog_obj ?
 always @(posedge clk) begin
     if( ioctl_addr==26'h19910 && ioctl_wr )
         game_id <= ioctl_dout==6;
-end
-
-always @(posedge clk, posedge rst) begin
-    if( rst ) begin
-        mcu_en   <= 0;
-        mcu_lock <= 0;
-    end else begin
-        if( prom_we && !mcu_lock ) begin
-            mcu_lock <= 1;
-            mcu_en   <= prog_data[7:0] != 8'hff;
-        end
-    end
 end
 
 jtframe_dwnld #(
@@ -361,10 +348,10 @@ jtsf_main #( .MAINW(MAINW), .RAMW(RAMW) ) u_main (
 `ifndef NOMCU
     jtsf_mcu u_mcu(
         .rst        ( rst24     ),
+        .clk        ( clk24     ),
         .clk_rom    ( clk       ),
         .rst_cpu    ( rst       ),
         .clk_cpu    ( clk       ),
-        .clk        ( clk24     ),
         // Main CPU interface
         .mcu_din    ( mcu_din   ),
         .mcu_dout   ( mcu_dout  ),