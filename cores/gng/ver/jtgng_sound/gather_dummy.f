2ac25aff0fe19934812d162df3189761507cf911
Jose Tejada <jose.tejada@jotego.es>
2024-04-01 11:56:44 +0200

    deleted deprecated sim

--- cores/gng/ver/jtgng_sound/gather_dummy.f
+++
@@ -1,9 +0,0 @@
-jt03_dummy.v
-../../hdl/jtgng_sound.v
-../../hdl/jtframe_ram.v
-../../../modules/tv80/tv80_alu.v
-../../../modules/tv80/tv80_core.v
-../../../modules/tv80/tv80_mcode.v
-../../../modules/tv80/tv80n.v
-../../../modules/tv80/tv80_reg.v
-../../../modules/tv80/tv80s.v
--- cores/gng/ver/jtgng_sound/ram_test.s
+++
@@ -1,9 +0,0 @@
-        LD A,0
-        LD HL, $C000
-LOOP:   INC A
-        LD B,A
-        LD (HL),B
-        LD B,0
-        LD B,(HL)
-        INC HL
-        JP LOOP
