c8e5db5f6bcd84926c7da8be59724dbbefd57430
Jose Tejada <jose.tejada@jotego.es>
2024-04-16 21:45:57 +0200

    run sim.sh instead of jtsim if present

--- modules/jtframe/bin/jtsim-all.sh
+++ modules/jtframe/bin/jtsim-all.sh
@@ -8,8 +8,12 @@ cat > $RUN_FOLDER <<EOF
 #!/bin/bash
 cd \$1
 pwd
+SIM=jtsim
+if [ -e sim.sh ]; then
+	SIM=sim.sh
+fi
 for SCN in \`find scenes -maxdepth 1 -mindepth 1 -type d | xargs -l1 basename\`; do
-	jtsim -batch -s \$SCN
+	\$SIM -batch -s \$SCN
 done
 EOF
 chmod +x $RUN_FOLDER
