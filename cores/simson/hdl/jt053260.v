jotego/jtcores

commit 9da157cad32dd52160e4bed4a9cb47031f8481d1

simson: PCM problems in vendetta fixed (#621) but creates regression for PCM ROM checks (#557)

cores/simson/hdl/jt053260.v
@@ -436,7 +436,7 @@ module jt053260_channel(
             if( cnt_up ) cnt <= cnt - 1'd1;
             if( !keyon ) begin
                 if(!tst_en || we) begin
-                    rom_addr <= start;
+                    rom_addr <= start+21'd1;
                 end else if( tst_en && tst_nx ) begin
                     rom_addr <= rom_addr+1'd1;
                 end

commit 5675d709eb3c7c9a638eaaec6d1adce66752478b

fixes regression for #557 introduced in 9da157ca

cores/simson/hdl/jt053260.v
@@ -436,7 +436,8 @@ module jt053260_channel(
             if( cnt_up ) cnt <= cnt - 1'd1;
             if( !keyon ) begin
                 if(!tst_en || we) begin
-                    rom_addr <= start+21'd1;
+                    // the first byte is not ADPCM data
+                    rom_addr <= start+{20'd0,~tst_en};
                 end else if( tst_en && tst_nx ) begin
                     rom_addr <= rom_addr+1'd1;
                 end