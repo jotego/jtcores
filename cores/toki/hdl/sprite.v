82f4d67558875bad7823b2cf5b134fdf5ee98b35
Solal Jacob <jacob.solal@gmail.com>
2024-04-22 13:02:29 +0200

    Fixes jotego/jtcores#605
    Fix hold & setup slack on pocket @96mhz

--- cores/toki/hdl/sprite.v
+++ cores/toki/hdl/sprite.v
@@ -23,10 +23,10 @@ module scan_sprite_ram(
   output                used_out
 );
 
-reg  [255:0] used = 256'b0;
+(* ramstyle = "no_rw_check" *)reg  [255:0] used = 256'b0;
 assign used_out = used[line_buffer_addr];
 
-reg [7:0] line_buffer [255:0];
+(* ramstyle = "no_rw_check" *)reg [7:0] line_buffer [255:0];
 assign line_buffer_out = line_buffer[line_buffer_addr];
 
 /// STATE MACHINE
@@ -40,7 +40,6 @@ parameter STATE_FETCH_ROM_WORDS = 4'd5;
 parameter STATE_COPY_ROM_WORDS = 4'd6;
 parameter STATE_COPY_PIXEL = 4'd7;
 parameter STATE_FINISHED = 4'd8;
-parameter STATE_WAIT = 4'd9;
 
 reg  [7:0]  previous_line_number;
 
@@ -64,63 +63,37 @@ assign plane2[15:0] = { rom_words[3][11:8],  rom_words[2][11:8],  rom_words[1][1
 assign plane3[15:0] = {  rom_words[3][7:4],   rom_words[2][7:4],   rom_words[1][7:4],    rom_words[0][7:4]};
 assign plane4[15:0] = {  rom_words[3][3:0],   rom_words[2][3:0],   rom_words[1][3:0],    rom_words[0][3:0]};
 
+wire [8:0]  line_buffer_index;
+assign line_buffer_index[8:0] = flip_x ?  x[8:0] + 8'd15 - {5'b0, pix_index} : 
+                                          x[8:0] + {5'b0, pix_index};
+
+wire [3:0]  plane_color;
+assign plane_color[3:0] = {plane1[pix_index], plane2[pix_index], plane3[pix_index], plane4[pix_index]};
+
 always @(posedge clk, posedge rst) begin
     if (rst) begin
       previous_line_number <= 8'd0;
       used  <= 256'b0;
       flip_x <= 1'd0;
-      color <= 4'd0;
-      x <= 9'd0;
-      y <= 9'd0;
-  
       pix_index <= 4'd0;
       rom_index <= 13'd0;
       rom_words_index <= 2'd0;
-      rom_words[0] <= 16'b0;
-      rom_words[1] <= 16'b0;
-      rom_words[2] <= 16'b0;
-      rom_words[3] <= 16'b0;
       ram_words_index <= 2'b0;
-      ram_words[0] <= 16'b0;
-      ram_words[1] <= 16'b0;
-      ram_words[2] <= 16'b0;
-      ram_words[3] <= 16'b0;
-      rom_words_index <= 2'd0;
       ram_addr <= 10'h0;
       state <= STATE_START;
       end
     else begin
     case (state)
       STATE_START : begin
-        used  <= 256'b0;
         previous_line_number <= line_number;
         used[255:0] <= 256'b0;
-        flip_x <= 1'd0;
-        color <= 4'd0;
-        x <= 9'd0;
-        y <= 9'd0;
         pix_index <= 4'd0;
-
         rom_index <= 13'd0;
         rom_words_index <= 2'd0;
-        rom_words[0] <= 16'b0;
-        rom_words[1] <= 16'b0;
-        rom_words[2] <= 16'b0;
-        rom_words[3] <= 16'b0;
         ram_words_index <= 2'b0;
-        ram_words[0] <= 16'b0;
-        ram_words[1] <= 16'b0;
-        ram_words[2] <= 16'b0;
-        ram_words[3] <= 16'b0;
-        rom_words_index <= 2'd0;
         ram_addr <= 10'h0;
-        //wait 1 cycle, for ram fetch
-        state <= STATE_WAIT;
+        state <= STATE_FETCH_RAM_WORDS;
       end      
-
-      STATE_WAIT : begin
-        state <=  STATE_FETCH_RAM_WORDS;
-      end
 
       STATE_FETCH_RAM_WORDS : begin
          ram_words[ram_words_index] <= ram_out[15:0];
@@ -191,53 +164,21 @@ always @(posedge clk, posedge rst) begin
       end
 
       STATE_COPY_PIXEL: begin
-          if (!flip_x && x[8:0] + {5'b0, pix_index} > 9'd256) begin 
-              if (pix_index < 15) begin
-                pix_index <= pix_index + 4'd1;
-                state <= STATE_COPY_PIXEL; 
-                end 
-              else 
-                state <= STATE_FETCH_RAM_WORDS;
-              end
-          else if (flip_x && (x[8:0] + 9'd15 - {5'b0, pix_index} > 9'd256)) begin 
-              if (4'd15 -pix_index >= 1) begin
-                pix_index <= pix_index + 4'd1;
-                state <= STATE_COPY_PIXEL; 
-                end 
-              else 
-                state <= STATE_FETCH_RAM_WORDS;
-              end
-          else begin
-            if ({plane1[pix_index],plane2[pix_index],plane3[pix_index],plane4[pix_index]} != 15) begin    
-              if (flip_x) begin
-                 if (x[8:0] + (9'd15 - {5'b0, pix_index[3:0]}) < 9'd256) begin 
-                    if (used[x[7:0] + (8'd15 - {4'b0, pix_index})] == 1'b0) begin
-                      line_buffer[x[7:0] + (8'd15 -  {4'b0, pix_index})] <= {color[3:0], {plane1[pix_index],plane2[pix_index],plane3[pix_index],plane4[pix_index]}};
-                      used[x[7:0] + (8'd15 - {4'b0, pix_index})] <= 1'b1;
-                    end
-                  end
-              end
-              else begin
-                if (x[8:0] + {5'b0, pix_index[3:0]} < 9'd256) begin 
-                  if (used[x[7:0] + {4'b0, pix_index}] == 1'b0) begin
-                    line_buffer[x[7:0] +  {4'b0, pix_index}] <= {color[3:0], {plane1[pix_index],plane2[pix_index],plane3[pix_index],plane4[pix_index]}};
-                    used[x[7:0] + {4'b0, pix_index}] <= 1'b1;
-                  end
-                end
-             end
-          end
-         
-          if (pix_index < 15) begin
-            pix_index <= pix_index + 4'd1;
-            state <= STATE_COPY_PIXEL; end              
-          else if (ram_addr == 'h3ff) begin
-            state <= STATE_FINISHED; end
-          else
-            state <= STATE_FETCH_RAM_WORDS;
+        if ((line_buffer_index < 9'd256) && (plane_color != 15) && (used[line_buffer_index[7:0]] == 1'b0)) begin   
+          line_buffer[line_buffer_index[7:0]] <= {color[3:0], plane_color};
+          used[line_buffer_index[7:0]] <= 1'b1;
           end
 
-      end 
-  
+        if (pix_index < 15) begin
+          pix_index <= pix_index + 4'd1;
+          state <= STATE_COPY_PIXEL; 
+          end              
+        else if (ram_addr == 'h3ff) 
+          state <= STATE_FINISHED; 
+        else
+          state <= STATE_FETCH_RAM_WORDS;
+      end
+
       STATE_FINISHED: begin
         ram_addr <= 10'h0;
         if (previous_line_number != line_number)
