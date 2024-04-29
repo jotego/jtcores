5c96a2c52e1a1a6dbf06a976a32c9f1f8c7910e2
Jose Tejada <jose.tejada@jotego.es>
2024-04-29 10:35:50 +0200

    s18: sch wip. shanon: deleted edit done by mistake

--- cores/s18/hdl/ym_lib.v
+++ cores/s18/hdl/ym_lib.v
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023 nukeykt
+ *
+ * This file is part of Nuked-MD.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 // ym3438, ym7101, fc1004 common cells
 
 module ym_sr_bit #(parameter SR_LENGTH = 1)
