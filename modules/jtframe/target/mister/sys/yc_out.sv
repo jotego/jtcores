412122082e53d7da2bc0f9760d17dabe16b224ce
Rafael Paiva <rafael.paiva@jotego.es>
2024-07-25 16:34:56 +0200

    Fixed issue with Pal visualization in Analogizer (greenish image)

--- modules/jtframe/target/mister/sys/yc_out.sv
+++ modules/jtframe/target/mister/sys/yc_out.sv
@@ -202,12 +202,12 @@ always_ff @(posedge clk) begin
 
 			// Calculate for chroma (Note: "PAL SWITCH" routine flips V * COS(Wt) every other line)
 		if (PAL_EN) begin
-			if (PAL_FLIP) begin
+			if (PAL_FLIP)
 				phase[4].c <= {12'b0,vref} + phase[3].u - phase[3].v;
-			 end else begin
+			  else
 				phase[4].c <= {12'b0,vref} + phase[3].u + phase[3].v;
 				PAL_line_count <= 1'd1;
-		end end else
+		end else
 				phase[4].c <= {12'b0,vref} + phase[3].u + phase[3].v;
 	end
 
