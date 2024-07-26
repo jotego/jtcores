da68ec49336dfe00d2ce62efa30bf9d90c95cdf5
Jose Tejada <jose.tejada@jotego.es>
2024-07-26 22:35:54 +0200

    riders beta - wip

--- modules/jtframe/bin/jotego_20.04.sh
+++ modules/jtframe/bin/jotego_20.04.sh
@@ -29,7 +29,7 @@ sudo apt install --yes libqwt-qt5-dev libsdl2-dev libfontconfig1-dev libsdl2-ttf
     qtchooser qt5-qmake
 
 # jtcore and jtupdate
-sudo apt install --yes parallel locate python3-pip fatattr sshpass
+sudo apt install --yes parallel locate python3-pip fatattr sshpass gawk libxml2-utils
 sudo updatedb
 
 # KiCAD
