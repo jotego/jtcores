f5f334a0c84988714a1b396de2b503a1c3dbf256
Jose Tejada <jose.tejada@jotego.es>
2024-08-14 08:44:19 +0200

    ngp: correcly mapped select gamepad button as power button in default mister configuration

--- cores/ngp/hdl/jtngp_game.v
+++ cores/ngp/hdl/jtngp_game.v
@@ -63,7 +63,6 @@ always @(posedge clk) begin
             default: st_mux <= 0;
         endcase
     endcase
-    st_mux <= { cab_1p[1:0], coin[1:0], 1'b0,joystick1[6:4] };
 end
 /* verilator tracing_on */
 jtngp_main u_main(
