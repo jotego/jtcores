f0688f39b41361828a7fb1028492073884f629a6
Jose Tejada <jose.tejada@jotego.es>
2024-08-20 08:20:29 +0200

    automatically create the rom.bin link when the setname can be derived from the folder name and the .rom file exists in $ROM

---
+++ cores/riders/ver/game/dump_split.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+
+SCENE=
+FNAME=
+NVRAM=0
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -s|--scene)
+            shift
+            SCENE=$1;;
+        -f|--file)
+			shift
+			FNAME=$1;;
+		-v|--nvram)
+			NVRAM=1;;
+        *) OTHER="$OTHER $1";;
+    esac
+    shift
+done
+
+if [ -z "$SCENE" ]; then
+	rm -f {scr?,pal,obj_??,???_mmr}.bin
+	exit 0
+fi
+
+if [ -z "$FNAME" ]; then
+	FNAME=$(basename $(pwd))
+	FNAME=${FNAME^^}
+	if [ $FNAME = GAME ]; then
+		echo "Cannot determine game name. Using dump.bin for scene data"
+		FNAME=dump.bin
+	else
+		FNAME=${FNAME}.RAM
+		echo "Using $FNAME as file name for scene data"
+	fi
+fi
+
+# The first 128 bytes are NVRAM
+if [ $NVRAM = 1 ]; then
+	TMP=`mktemp`
+	dd if=scenes/$SCENE/$FNAME of=nvram.bin bs=128 count=1 2> /dev/null
+	dd if=scenes/$SCENE/$FNAME of=$TMP      bs=128 skip=1  2> /dev/null
+else
+	TMP=$FNAME
+fi
+dd if=$TMP of=scr1.bin count=16         2> /dev/null
+dd if=$TMP of=scr0.bin count=16 skip=16 2> /dev/null
+dd if=$TMP of=pal.bin  count=8  skip=32 2> /dev/null
+jtutil drop1    < pal.bin > pal_lo.bin
+jtutil drop1 -l < pal.bin > pal_hi.bin
+dd if=$TMP      of=obj.bin count=16 skip=40   				  2> /dev/null
+dd if=/dev/zero of=obj.bin count=16 conv=notrunc oflag=append 2> /dev/null
+jtutil drop1 -l < obj.bin > obj_lo.bin
+jtutil drop1    < obj.bin > obj_hi.bin
+# MMR
+dd if=$TMP of=pal_mmr.bin bs=8 count=2 skip=$((56*512/8))   2> /dev/null
+dd if=$TMP of=scr_mmr.bin bs=8 count=1 skip=$((56*512/8+2)) 2> /dev/null
+dd if=$TMP of=obj_mmr.bin bs=8 count=1 skip=$((56*512/8+3)) 2> /dev/null
+dd if=$TMP of=dim.bin     bs=1 count=1 skip=$((56*512+4*8)) 2> /dev/null
+
+if [ $NVRAM = 1 ]; then rm -f $TMP; fi
