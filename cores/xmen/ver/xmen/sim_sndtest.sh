76be4c1fb0d1516ae93d63448263148bed44b4be
Jose Tejada <jose.tejada@jotego.es>
2024-10-07 08:31:31 +0200

    xmen: some samples sound correctly

---
+++ cores/xmen/ver/xmen/sim_sndtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+jtutil sdram
+LAST=540
+# voice test 1 finishes at 447
+# voice test 2 finishes at 540
+while [ $# -gt 0 ]; do
+    case "$1" in
+    	1) LAST=447;;
+		2) LAST=540;;
+		*) echo "Unknown argument $1"; exit 1;;
+	esac
+	shift
+done
+
+  jtsim -inputs snd_test.in -video $LAST -w 332
