697c1b6b3adaa4bcd75a63bfc5a3af1fcb722ace
JOTEGO <jose.tejada@jotego.es>
2024-11-19 09:43:55 +0100

    try six different seeds (instead of four)

--- modules/jtframe/devops/xjtcore.sh
+++ modules/jtframe/devops/xjtcore.sh
@@ -42,7 +42,7 @@ if [ -e $CORES/$CORENAME/cfg/macros.def ]; then
         if [ $TARGET != pocket ]; then SKIPPOCKET=--skipPocket; else unset SKIPPOCKET; fi
         jtframe mra $NODBG --skipROM $SKIPPOCKET $CORENAME
         echo "Compiling for $TARGET"
-        jtseed 4 $CORENAME -$TARGET $NODBG
+        jtseed 6 $CORENAME -$TARGET $NODBG
         # recover hard disk space
         rm -rf $CORES/$CORENAME/$TARGET
     done
