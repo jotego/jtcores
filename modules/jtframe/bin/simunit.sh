24e1133a8685f1f87c1da4847295723a66fd95b5
Jose Tejada <jose.tejada@jotego.es>
2024-12-31 17:50:01 +0100

    simunit workflow

--- modules/jtframe/bin/simunit.sh
+++ modules/jtframe/bin/simunit.sh
@@ -2,6 +2,14 @@
 # use to simulate unit tests
 # create a file called gather.f with the names of the files to include in the sim
 # not local to the simulation folder
+
+RUNFOLDER="$1"
+if [ -z "$RUNFOLDER" ]; then
+	RUNFOLDER=`pwd`
+fi
+
+cd $RUNFOLDER
+
 iverilog `find -name "*.v"` `find -name "*.sv"` $JTFRAME/hdl/{video/jtframe_vtimer.v,ver/jtframe_test_clocks.v} -fgather.f -s test -o sim -D SIMULATION && sim -lxt > sim.log
 rm -f sim
 if grep PASS sim.log > /dev/null; then
