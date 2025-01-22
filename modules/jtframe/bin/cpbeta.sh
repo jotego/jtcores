c932946243223d52b97eaeefc748afb25940f9d2
Jose Tejada <jose.tejada@jotego.es>
2025-01-22 22:08:03 +0100

    cpbeta.sh refactored

--- modules/jtframe/bin/cpbeta.sh
+++ modules/jtframe/bin/cpbeta.sh
@@ -21,30 +21,39 @@
 # Places a copy of the files in $JTROOT and another one, for
 # archiving purposes in $JTFRIDAY
 
-FRIDAY="friday"
 
-function last_friday {
-    local LAST=`date --date="last friday" +"%y%m%d"`
-    local PUBLISHED=$(find $JTFRIDAY -maxdepth 1 -type d -regextype posix-extended -regex ".*/[0-9]{6}$" | sort --reverse | head -n 1)
-    if [ -z "PUBLISHED" ]; then
-        echo "last friday"
-        return
-    fi
-    PUBLISHED=$(basename $PUBLISHED)
-    if [ $PUBLISHED -gt $LAST ]; then
-        echo "Using last published date ($PUBLISHED)" > /dev/stderr
-        echo ${PUBLISHED:0:2}-${PUBLISHED:2:2}-${PUBLISHED:4:2}
-    else
-        echo "last friday"
-    fi
+main() {
+    parse_args $*
+    check_jtfriday_defined
+
+    make_temp_folder
+    copy_all_targets
+
+    make_zip_files
+    archive_zip_files
+    clean_up
 }
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --last)
-            FRIDAY="last friday";;
-        -h|--help)
-            cat<<EOF
+parse_args() {
+    FRIDAY="friday"
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            --last)
+                FRIDAY="last friday";;
+            -h|--help)
+                show_help
+                exit 0;;
+            *)
+                echo "Unknown argument $1"
+                exit 1;;
+        esac
+        shift
+    done
+    SHORTSTAMP=$(date --date="$FRIDAY" +"%y%m%d")
+}
+
+show_help() {
+    cat<<EOF
 cpbeta.sh <hash> [arguments]
 
 Creates zip files for distribution on JTFRIDAY.
@@ -54,68 +63,79 @@ Creates zip files for distribution on JTFRIDAY.
                 or latest published date, if it is more recent
                 than last friday's
 EOF
-            exit 0;;
-        *)
-            echo "Unknown argument $1"
-            exit 1;;
-    esac
-    shift
-done
+}
 
-SHORTSTAMP=$(date --date="$FRIDAY" +"%y%m%d")
-DEST=`mktemp --directory`
-UPMR=
-
-if [ ! -d "$JTFRIDAY" ]; then
-    cat >/dev/stderr <<EOF
+check_jtfriday_defined() {
+    if [ ! -d "$JTFRIDAY" ]; then
+        cat <<EOF
 Missing JTFRIDAY variable pointing to the destination folder
 Define it if you want to archive a copy of the zip files there
 EOF
-    exit 1
-fi
+        exit 1
+    fi
+}
 
-# MiSTer
-mkdir -p "$DEST"/_Arcade/cores "$DEST/mra"
-mkdir -p $DEST/games/mame
-cp $JTUTIL/jtbeta.zip $DEST/games/mame
-cp $JTBIN/mister/*rbf "$DEST"/_Arcade/cores
+make_temp_folder() {
+    DEST=`mktemp --directory`
+}
 
-# MiST, SiDi
-cp -r $JTBIN/mra/* "$DEST"/_Arcade
-cp -r $JTBIN/{mist,sidi,sidi128,mra} "$DEST"
+copy_all_targets() {
+    copy_mister_files
+    copy_mist_sidi_files
+    copy_pocket_files
+}
 
-# Pocket
-mkdir -p $DEST/pocket/Assets/jtpatreon/common
-cp -r $JTBIN/pocket/raw/* $DEST/pocket
-unzip -q $JTUTIL/jtbeta.zip -d $DEST/pocket/Assets/jtpatreon/common
-mkdir $DEST/pocket/readme
-mv $DEST/pocket/*.txt $DEST/pocket/readme
+copy_mister_files() {
+    mkdir -p "$DEST"/_Arcade/cores "$DEST/mra"
+    mkdir -p $DEST/games/mame
+    cp $JTUTIL/jtbeta.zip $DEST/games/mame
+    cp $JTBIN/mister/*rbf "$DEST"/_Arcade/cores
+}
 
-# Make zip files
-cd $DEST
-cat >zip_comment <<EOF
+copy_mist_sidi_files() {
+    cp -r $JTBIN/mra/* "$DEST"/_Arcade
+    cp -r $JTBIN/{mist,sidi,sidi128,mra} "$DEST"
+}
+
+copy_pocket_files() {
+    mkdir -p $DEST/pocket/Assets/jtpatreon/common
+    cp -r $JTBIN/pocket/raw/* $DEST/pocket
+    unzip -q $JTUTIL/jtbeta.zip -d $DEST/pocket/Assets/jtpatreon/common
+    mkdir $DEST/pocket/readme
+    mv $DEST/pocket/*.txt $DEST/pocket/readme
+}
+
+make_zip_files() {
+    cd $DEST
+    cat >zip_comment <<EOF
       -- JOTEGO --
 
 Beta files for Patreon supporters.
   https://www.patreon.com/jotego
 
 EOF
-function betazip {
+    betazip jtfriday_${SHORTSTAMP}_mister.zip _Arcade games &
+    betazip jtfriday_${SHORTSTAMP}_pocket.zip mra pocket &
+    betazip jtfriday_${SHORTSTAMP}_other.zip  mra mist sidi* &
+    wait
+    cp *.zip $JTROOT
+}
+
+betazip() {
     cat $DEST/zip_comment | zip -qr --test --archive-comment -9 $*
 }
 
-betazip jtfriday_${SHORTSTAMP}_mister.zip _Arcade games &
-betazip jtfriday_${SHORTSTAMP}_pocket.zip mra pocket &
-betazip jtfriday_${SHORTSTAMP}_other.zip  mra mist sidi* &
 
-wait
-cp *.zip $JTROOT
-if [ -d "$JTFRIDAY" ]; then
+archive_zip_files() {
     ZIPDEST="$JTFRIDAY"/$SHORTSTAMP
     mkdir -p "$ZIPDEST"
     cp *.zip "$ZIPDEST"
     cp $JTUTIL/jtbeta.zip "$ZIPDEST"
-fi
+}
 
-cd $JTROOT
-rm -rf $DEST
+clean_up() {
+    cd $JTROOT
+    rm -rf $DEST
+}
+
+main $*
