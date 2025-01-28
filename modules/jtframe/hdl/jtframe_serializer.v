9c67f498d29e2bd5ab8067808ceee6f57dbcf681
Rafael Paiva <rafael.paiva@jotego.es>
2025-01-28 13:30:58 +0100

    Serializer module and test added to jtframe

---
+++ modules/jtframe/hdl/jtframe_serializer.v
@@ -0,0 +1,58 @@
+/*  This file is part of JTCORES.
+    JTCORES program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JTCORES program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with JTCORES.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Rafael Eduardo Paiva Feener. Copyright: Miki Saito
+    Version: 1.0
+    Date: 24-01-2025 */
+
+module jtframe_serializer#(parameter DW=8, PAR=1)( // Set PAR==0 for even parity
+    input            rst,
+    input            clk,
+    input            cen,
+    input   [DW-1:0] din,
+    input            load,
+    output           sdout,
+    output reg       sclk
+);
+
+localparam CK=$clog2(DW+2);
+reg  [DW+1:0] pre_data;
+reg  [CK-1:0] cnt;
+wire done, par;
+
+assign done  = cnt==0;
+assign sdout = pre_data[0];
+assign par = ^din ^(PAR==1);
+
+always @(posedge clk) begin 
+    if(rst) begin
+        sclk     <= 0;
+        pre_data <= {DW+2{1'b1}};
+        cnt <= 0;
+    end else if(cen) begin
+        sclk <= ~sclk;
+        if(!done ) begin
+            if( !sclk ) begin
+                pre_data <= {1'b1,pre_data[DW+1:1]};
+                cnt <= cnt-1'b1;
+            end
+        end
+        if( load  ) begin
+            pre_data <= {par,din, 1'b0};
+            cnt <= DW+2'd2;
+        end
+    end
+end
+
+endmodule
---
+++ modules/jtframe/ver/serializer/test.v
@@ -0,0 +1,74 @@
+module test;
+
+localparam DW=8;
+
+reg clk, rst;
+
+reg  [   7:0] cen_cnt=1;
+wire [DW-1:0] data_in;
+wire [DW-1:0] data_out;
+reg        load;
+wire       sd, valid, oddpar, sclk, cen;
+integer    value=0;
+
+
+initial begin
+    $dumpfile("test.lxt");
+    $dumpvars;
+    $dumpon;
+    #2000000
+    $display("FAIL");
+    $finish;
+end
+
+
+initial begin
+    clk=0;
+    forever #10 clk=~clk;
+end
+
+always @(posedge clk) cen_cnt <= {cen_cnt[0],cen_cnt[7:1]};
+
+assign cen     = cen_cnt[0];
+assign oddpar  = ^value[7:0] ^1'b1;
+assign data_in = {oddpar,value[0+:DW]};
+
+initial begin
+    rst    = 1;
+    load   = 0;
+    repeat (20) @(posedge clk);
+    rst  = 0;
+    repeat (20) @(posedge clk);
+    repeat (100) begin
+        value  = $random;
+        wait ( cen==1 && !sclk );
+        load    = 1;
+        repeat (8) @(posedge clk);
+        load    = 0;
+        wait ( valid && data_in == data_out );
+    end
+    repeat (10) @(posedge clk);
+    $display("PASS");
+    $finish;
+end
+
+jtframe_serializer#(.DW(DW)) uut (
+    .clk       ( clk      ),
+    .rst       ( rst      ),
+    .cen       ( cen      ),
+    .din       ( data_in  ),
+    .load      ( load     ),
+    .sdout     ( sd       ),
+    .sclk      ( sclk     )
+);
+
+ps2_intf_v uut_s (
+    .CLK      ( clk      ),
+    .nRESET   ( ~rst     ),
+    .PS2_CLK  ( sclk     ),
+    .PS2_DATA ( sd       ),
+    .DATA     ( data_out ),
+    .VALID    ( valid    ),
+    .ERROR    (          )
+);
+endmodule
