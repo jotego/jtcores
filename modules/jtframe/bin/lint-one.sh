00a0bf43fbba9686ed386c060378cfb1be2640d4
Rafael Paiva <rafael.paiva@jotego.es>
2025-01-28 10:14:08 +0100

    refactored lint-one.sh

--- modules/jtframe/bin/lint-one.sh
+++ modules/jtframe/bin/lint-one.sh
@@ -1,35 +1,68 @@
-#!/bin/bash
+#!/bin/bash -e
 # use lint-one.sh core -u JTFRAME_SKIP
 # for cores in development phase
 
-CORE=$1
-shift
-if [ -z "$TARGET" ]; then
-    TARGET=mister
-fi
+trap "clean_up; exit 1" INT KILL
 
-eval `jtframe cfgstr $CORE --output bash --target $TARGET $*`
+main() {
+    CORE=$1
+    shift
+    set_target
+    read_core_macros
 
-if [[ ! -e $CORES/$CORE/cfg/macros.def || -e $CORES/$CORE/cfg/skip || -v JTFRAME_SKIP ]]; then
-    echo "Skipping $CORE"
-    exit 0
-fi
+    if must_skip; then
+        echo "Skipping $CORE"
+        exit 0
+    fi
 
-cd $CORES/$CORE
-TEST_FOLDER=ver/lint
-rm -rf $TEST_FOLDER
-mkdir -p $TEST_FOLDER
-cd $TEST_FOLDER
+    prepare_test_folder
+    cd $TEST_FOLDER
+    make_dummy_rom
 
-if [ ! -e rom.bin ]; then
-    # dummy ROM
-    dd if=/dev/zero of=rom.bin count=1 2> /dev/null
-    DELROM=
-fi
+    run_linter
+    check_msg
+    clean_up
+}
 
-jtsim -lint -$TARGET $*
-EXIT=$?
-jtframe msg $CORE
-EXIT=$(($EXIT || $?))
-rm -rf $TEST_FOLDER
-exit $EXIT
+set_target() {
+    if [ -z "$TARGET" ]; then
+        TARGET=mister
+    fi
+}
+
+read_core_macros() {
+    eval `jtframe cfgstr $CORE --output bash --target $TARGET $*`
+}
+
+must_skip() {
+    [[ ! -e $CORES/$CORE/cfg/macros.def || -e $CORES/$CORE/cfg/skip || -v JTFRAME_SKIP ]]
+}
+
+prepare_test_folder() {
+    cd $CORES/$CORE
+    TEST_FOLDER=ver/lint
+    rm -rf $TEST_FOLDER
+    mkdir -p $TEST_FOLDER
+}
+
+make_dummy_rom() {
+    if [ ! -e rom.bin ]; then
+        # dummy ROM
+        dd if=/dev/zero of=rom.bin count=1 2> /dev/null
+        DELROM=
+    fi
+}
+
+run_linter() {
+    jtsim -lint -$TARGET $*
+}
+
+check_msg() {
+    jtframe msg $CORE
+}
+
+clean_up() {
+    rm -rf $TEST_FOLDER
+}
+
+main $*
