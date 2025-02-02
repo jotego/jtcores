a904239c5e8f4f7ab22343719270be9ef6f3a373
Jose Tejada <jose.tejada@jotego.es>
2025-02-02 09:35:53 +0100

    jtsim: -dipsw parsing fixed

--- modules/jtframe/bin/jtsim-funcs-test.sh
+++ modules/jtframe/bin/jtsim-funcs-test.sh
@@ -4,44 +4,78 @@ source $JTFRAME/bin/jtsim-funcs
 
 main() {
     test_get_macro
+    test_has_macro
     if [ "$FAIL" = 1 ]; then
         exit 1
     fi
 }
 
 fail() {
-    local veredict="$1"
-    shift
     local msg="$*"
     local test_name=${FUNCNAME[1]}
-    printf "%-20s %6s %s\n" $test_name $veredict "$msg"
-    if [ "$veredict" = "FAIL" ]; then
-        FAIL=1
-    fi
+    printf "%-20s FAIL %s\n" $test_name "$msg"
+    FAIL=1
 }
+
+pass() {
+    local bad="$1"
+    if [ ! -z "$bad" ]; then
+        return
+    fi
+    local test_name=${FUNCNAME[1]}
+    printf "%-20s PASS\n" $test_name
+}
+
+test_has_macro() {
+    local bad
+    ALLMACROS="a,d=,b=3,MAXFRAME=23,c=4"
+    if has_macro not_there; then
+        fail "found non existing macro"
+        bad=1
+    fi
+    if ! has_macro MAXFRAME; then
+        fail "coult not find MAXFRAME"
+        bad=1
+    fi
+    pass $bad
+}
+
 
 test_get_macro() {
     ALLMACROS="a,d=,b=3,MAXFRAME=23,c=4"
+    local bad
 
     if [ ! -z `get_macro` ]; then
-        fail FAIL empty case. Got -`get_macro`-
+        fail empty case. Got -`get_macro`-
+        bad=1
     fi
 
     if [ ! -z `get_macro a` ]; then
-        fail FAIL a. Got `get_macro a`
+        fail a. Got `get_macro a`
+        bad=1
     fi
 
     if [ ! -z `get_macro d` ]; then
-        fail FAIL d
+        fail d
+        bad=1
     fi
 
     if [ "`get_macro b`" != 3 ]; then
-        fail FAIL "b expected 3 got" `get_macro b`
+        fail "b expected 3 got" `get_macro b`
+        bad=1
     fi
 
     if [ "`get_macro MAXFRAME`" != 23 ]; then
-        fail FAIL "MAXFRAME expected 23 got" `get_macro MAXFRAME`
+        fail "MAXFRAME expected 23 got" `get_macro MAXFRAME`
+        bad=1
     fi
+
+    ALLMACROS="JTFRAME_SIM_DIPS=0xf0"
+    if [ -z "`get_macro JTFRAME_SIM_DIPS`" ]; then
+        fail "Cannot find JTFRAME_SIM_DIPS"
+        bad=1
+    fi
+    pass $bad
 }
 
 main $*
