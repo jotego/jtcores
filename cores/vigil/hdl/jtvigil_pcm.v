6309ead5a866bb1a261fc6b2eff54e11fdf9c05f
Jose Tejada <jose.tejada@jotego.es>
2025-02-14 10:39:11 +0100

    vigil: leave PCM output at zero when sample playback ends. Fixes #974

--- cores/vigil/hdl/jtvigil_pcm.v
+++ cores/vigil/hdl/jtvigil_pcm.v
@@ -16,6 +16,10 @@
     Version: 1.0
     Date: 1-5-2022 */
 
+// some PCM codes
+//      0020: punch
+//      0F20: kick
+//      0720: cry when hit
 module jtvigil_pcm(
     input             rst,
                       clk,
@@ -23,7 +27,7 @@ module jtvigil_pcm(
     input             hi_cs, lo_cs, cnt_up,
     input     [ 7:0]  din,
 
-    output reg        rom_cs,
+    output            rom_cs,
     output reg [15:0] rom_addr,
     input      [ 7:0] rom_data,
     input             rom_ok,    
@@ -31,28 +35,38 @@ module jtvigil_pcm(
     output reg signed [7:0] snd
 );
 
-wire signed [7:0] pcm_signed;
-reg cntup_l;
+localparam [7:0] SAMPLE_DONE=0;
 
-assign pcm_signed = 8'h80 - rom_data;
+reg cntup_l, up;
+
+assign rom_cs = 1;
+
+function signed [7:0] filter_clip_at_end(input [7:0]sample); begin
+    reg signed [7:0] pcm_signed;
+    reg is_sample_end;
+
+    pcm_signed         = 8'h80 - sample;
+    is_sample_end      = sample==SAMPLE_DONE;
+    filter_clip_at_end = is_sample_end ? 8'h0 : pcm_signed;
+end endfunction
 
 always @(posedge clk) begin
     if( rst ) begin
         rom_addr <= 16'd0;
         cntup_l  <= 0;
         snd      <= 0;
-        rom_cs   <= 0;
+        up       <= 0;
     end else begin
         cntup_l    <= cnt_up;
-        if( rom_cs && rom_ok ) begin
-            snd    <= pcm_signed;
-            rom_cs <= 0;
+        if( up && rom_ok ) begin
+            snd <= filter_clip_at_end(rom_data);
+            up  <= 0;
         end
-        if( hi_cs ) rom_addr[15:8] <= din;
-        if( lo_cs ) rom_addr[ 7:0] <= din;
+        if( hi_cs ) begin up <= 1; rom_addr[15:8] <= din; end
+        if( lo_cs ) begin up <= 1; rom_addr[ 7:0] <= din; end
         if( cnt_up && !cntup_l ) begin
             rom_addr <= rom_addr + 16'd1;
-            rom_cs   <= 1;
+            up       <= 1;
         end
     end
 end
