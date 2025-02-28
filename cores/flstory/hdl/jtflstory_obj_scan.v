7935e649de2f582e87fd39fbb4a52c9e8464c174
Jose Tejada <jose.tejada@jotego.es>
2025-02-28 15:46:04 +0100

    flstory: fixes sprite priority on nycaptor

--- cores/flstory/hdl/jtflstory_obj_scan.v
+++ cores/flstory/hdl/jtflstory_obj_scan.v
@@ -60,7 +60,7 @@ assign ram_din   = chk;
 assign ram_addr = vsbl  ? {4'b1101,cnt[2:0],cnt[3]&blink}: // visible indexes   D0~D7, blinking D8~DF
                   info  ? {1'b0,   chk[4:0],  obj_sub   }: // object data       00~7F
                   order ? {3'b100, scan                 }: // object draw order 80~9F
-                          {3'b101, colscr            }; // column scroll     A0~BF
+                          {3'b101, colscr               }; // column scroll     A0~BF
 assign ydiff    = vlatch+ram_dout;
 assign inzone   = ydiff[7:4] == 4'b1111;
 assign hf       = ghflip ? -9'h8-hdump : hdump;
@@ -75,7 +75,7 @@ always @(posedge clk) begin
     draw     <= 0;
     blank    <= vdump >= 9'h1f2 || vdump <= 9'h10e || !vdump[8];
     cen      <= ~cen;
-    if(!scan_done && cen) begin
+    if(!scan_done && cen ) begin
         if( order ) begin
             st <= {st[1:0],st[2]};
             case(st)
@@ -100,34 +100,32 @@ always @(posedge clk) begin
                     end
                 end
             endcase
-        end else begin
+        end else if(!dr_busy) begin
             if( !info  ) begin
                 chk      <= ram_dout;
                 pal[6:4] <= ram_dout[7:5]; // priority bits
                 info     <= 1;
                 vsbl     <= 0;
             end else begin
-                if(!dr_busy) begin
-                    obj_sub <= obj_sub+2'd1;
-                    case(obj_sub)
-                        0: begin
-                            ysub <= ydiff[3:0];
-                            inzone_l <= inzone;
-                        end
-                        1: begin
-                            {vflip,hflip,code[9:8],pal[3:0]} <= ram_dout;
-                            if(layout) code[9:8] <= {1'b0,ram_dout[5]};
-                        end
-                        2: code[7:0] <= ram_dout;
-                        3: begin
-                            draw <= inzone_l;
-                            info <= 0;
-                            vsbl <= 1;
-                            xpos <= ram_dout;
-                            {scan_done, cnt[2:0]} <= {1'b0,cnt[2:0]}-4'd1;
-                        end
-                    endcase
-                end
+                obj_sub <= obj_sub+2'd1;
+                case(obj_sub)
+                    0: begin
+                        ysub <= ydiff[3:0];
+                        inzone_l <= inzone;
+                    end
+                    1: begin
+                        {vflip,hflip,code[9:8],pal[3:0]} <= ram_dout;
+                        if(layout) code[9:8] <= {1'b0,ram_dout[5]};
+                    end
+                    2: code[7:0] <= ram_dout;
+                    3: begin
+                        draw <= inzone_l;
+                        info <= 0;
+                        vsbl <= 1;
+                        xpos <= ram_dout;
+                        {scan_done, cnt[2:0]} <= {1'b0,cnt[2:0]}-4'd1;
+                    end
+                endcase
             end
         end
     end
