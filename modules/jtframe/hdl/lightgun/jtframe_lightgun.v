dc13edff08cd081ee6e8783b97a59ea66a283372
Rafael Paiva <rafael.paiva@jotego.es>
2025-03-17 10:03:21 +0100

    minor

--- modules/jtframe/hdl/lightgun/jtframe_lightgun.v
+++ modules/jtframe/hdl/lightgun/jtframe_lightgun.v
@@ -1,4 +1,4 @@
-/*  This file is part of JTFRAME.
+q/*  This file is part of JTFRAME.
     JTFRAME program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -96,77 +96,5 @@ assign {cross1_x, cross1_y} = 18'b0;
 assign {cross2_x, cross2_y} = 18'b0;
 assign  cross_disable   =  2'd3;
 `endif
-
-endmodule
-
-module jtframe_lightgun_mux(
-    input         rst,
-    input         clk,
-    input         gun_border_en,
-    input  [ 1:0] rotate,
-    input  [15:0] joyana,
-    input  [15:0] mouse,
-    input         mouse_strobe,
-    output        strobe,
-    output [ 8:0] gun_x,
-    output [ 8:0] gun_y,
-    output [ 8:0] cross_x,
-    output [ 8:0] cross_y
-);
-
-parameter W = 384, H = 224, XOFFSET=0, YOFFSET=0;
-
-wire [8:0] mouse_x, mouse_y, joyana_x, joyana_y;
-wire [7:0] dx, dy;
-wire       a_strobe, mouse_strobe_dly;
-
-jtframe_mouse_rotation mouse_rot(
-    .clk          ( clk                 ),
-    .strobe       ( mouse_strobe        ),
-    .strobe_dly   ( mouse_strobe_dly    ),
-    .rotate       ( rotate              ),
-    .dx_in        ( mouse[ 7: 0]        ),
-    .dy_in        ( mouse[15: 8]        ),
-    .dx           ( dx                  ),
-    .dy           ( dy                  )
-);
-
-jtframe_mouse_abspos #(.W(W),.H(H)
-) crosshair_mouse(
-    .clk        ( clk              ),
-    .dx         ( dx               ),
-    .dy         ( dy               ),
-    .strobe     ( mouse_strobe_dly ),
-    .x          ( mouse_x          ),
-    .y          ( mouse_y          )
-);
-
-jtframe_joyana_abspos #(.W(W),.H(H)
-) crosshair_joyana(
-    .clk        ( clk             ),
-    .joyana     ( joyana          ),
-    .strobe     ( a_strobe        ),
-    .x          ( joyana_x        ),
-    .y          ( joyana_y        )
-);
-
-jtframe_lightgun_position #(
-    .XOFFSET(XOFFSET),.YOFFSET(YOFFSET)
-) crosshair_mux(
-    .rst        ( rst             ),
-    .clk        ( clk             ),
-    .gun_border_en( gun_border_en ),
-    .m_x        ( mouse_x         ),
-    .m_y        ( mouse_y         ),
-    .m_strobe   ( mouse_strobe    ),
-    .a_x        ( joyana_x        ),
-    .a_y        ( joyana_y        ),
-    .a_strobe   ( a_strobe        ),
-    .x          ( gun_x           ),
-    .y          ( gun_y           ),
-    .x_abs      ( cross_x         ),
-    .y_abs      ( cross_y         ),
-    .strobe     ( strobe          )
-);
 
 endmodule
