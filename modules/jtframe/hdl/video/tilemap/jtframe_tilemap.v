jotego/jtcores

commit 3a95fad19158f5ca46e933fbfb8a74efb1b88471

gaiden: fixes #913

modules/jtframe/hdl/video/tilemap/jtframe_tilemap.v
@@ -137,7 +137,7 @@ end
 
 reg heff_l, hmsb_l;
 wire zero = heff[3] != heff_l // read the ROM every 8 pixels
-         || (hmsb_l!=hdump[7] && HJUMP==0); // identify a typical blanking end case
+         || (hmsb_l!=hdump[HDUMPW-1] && HJUMP==0); // identify a typical blanking end case
 
 always @(posedge clk) begin
     if( rst ) begin
@@ -148,7 +148,7 @@ always @(posedge clk) begin
         hflip_g  <= 0;
     end else if(pxl_cen) begin
         heff_l <= heff[3];
-        hmsb_l <= hdump[7];
+        hmsb_l <= hdump[HDUMPW-1];
         if( zero ) begin
             rom_cs <= ~rst & blankn;
             rom_addr[0+:VW] <= veff[0+:VW]^{VW{vflip_g}};

cores/gaiden/hdl/jtgaiden_scroll.v
@@ -67,7 +67,13 @@ jtframe_bram_burst u_txtburst(
     .dout32 ( ram_full     )
 );
 
-jtframe_scroll #(.PW(9),.SIZE(16),.VA(11),.MAP_HW(10),.MAP_VW(9),.HJUMP(0)) u_scroll(
+localparam [8:0] DEEP_IN_HB=9'h120;
+
+jtframe_scroll #(
+    .PW(9),.SIZE(16),.VA(11),
+    .MAP_HW(10),.MAP_VW(9),
+    .HJUMP(0),.HLOOP(DEEP_IN_HB))
+u_scroll(
     .rst        ( rst       ),
     .clk        ( clk       ),
     .pxl_cen    ( pxl_cen   ),
@@ -75,7 +81,7 @@ jtframe_scroll #(.PW(9),.SIZE(16),.VA(11),.MAP_HW(10),.MAP_VW(9),.HJUMP(0)) u_sc
     .hs         ( hs        ),
     .vdump      ( vdump     ),
     .hdump      ( hdump     ),
-    .blankn     ( blankn    ),  // if !blankn there are no ROM requests
+    .blankn     ( 1'b1      ),
     .flip       ( flip      ),
     .scrx       ( scr_x[9:0]),
     .scry       ( scr_y[8:0]),

modules/jtframe/hdl/video/tilemap/jtframe_scroll.v
@@ -36,7 +36,7 @@ module jtframe_scroll #( parameter
     XOR_HFLIP = 0, // set to 1 so hflip gets ^ with flip
     XOR_VFLIP = 0, // set to 1 so vflip gets ^ with flip
     HJUMP     = 1, // set to 0 for linear hdump starting at zero after HB
-
+    HLOOP     = 0, // see jtframe_scroll_offset
     COL_SCROLL = 0 // set to 1 to enable 8-pixel column scroll
 )(
     input              rst,
@@ -79,6 +79,7 @@ jtframe_scroll_offset #(
     .MAP_VW     ( MAP_VW    ),
     .HDUMPW     ( HDUMPW    ),
     .VDUMPW     ( VDUMPW    ),
+    .HLOOP      ( HLOOP     ),
     .COL_SCROLL ( COL_SCROLL)
 ) u_offset(
     .clk        ( clk       ),

modules/jtframe/hdl/video/tilemap/jtframe_scroll_offset.v
@@ -21,7 +21,11 @@ module jtframe_scroll_offset #(parameter
     MAP_VW     = 9,
     VDUMPW     = 9,
     HDUMPW     = 9,
-    COL_SCROLL = 0 // set to 1 to enable 8-pixel column scroll
+    HLOOP      = 0, // when !=0, it is the value of hdump at which
+                    // hdump[8:7] are set to 1, and hdf uses &hdump[8:7] for MSBs
+                    // this is useful to have heff correctly operate at the
+                    // trasition from blanking to active
+    COL_SCROLL = 0  // set to 1 to enable 8-pixel column scroll
 )(
     input       clk, 
                 flip, hs,
@@ -33,22 +37,28 @@ module jtframe_scroll_offset #(parameter
     output reg [HDUMPW-1:0] heff
 );
 
-localparam VDW=9,
-           HEW = HDUMPW>VDW ? HDUMPW : VDW,
+localparam VDW=9, HDW=10,
+           HEW = HDUMPW>HDW ? HDUMPW : HDW,
            VEW = VDUMPW>VDW ? VDUMPW : VDW;
 
-reg  [VDW-1:0] vdf, hdf;
+reg  [VDW-1:0] vdf;
+reg  [HDW-1:0] hdf;
 reg  [HEW-1:0] hfull;
 reg  [VEW-1:0] vfull;
-wire h8;
+wire h8,blank;
 reg  hsl, h8_l,
      line_changed, tile_changed, update_veff;
+wire [8:0] hdfix = HLOOP==0 ? hdump :
+                   hdump>HLOOP ? {2'b11,hdump[6:0]} : hdump;
 
 assign h8 = heff[3];
+assign blank = HLOOP==0 ? hdump[8] : &hdfix[8:7];
 
 always @* begin
-    hdf   = hdump ^ { 1'b0, {8{flip}} };
-    hfull = hdf + scrx;
+    // hdf should make a perfect subtraction during blanking
+    // HLOOP can be used to help achieve that
+    hdf   = {blank,hdfix} ^ { 2'b0, {8{flip}} };
+    hfull = hdf + {{HDW-MAP_HW{1'b0}},scrx};
     heff  = hfull[HDUMPW-1:0];
 
     vdf   = vdump ^ { 1'b0, {8{flip}} };

commit e396eb68182c212cf3417aca67e4ff2ae7b4cd5d

Fixes #1005 after regression introduced in 3a95fad. Check with jtsim -s border in wwfss core

modules/jtframe/hdl/video/tilemap/jtframe_tilemap.v
@@ -137,7 +137,7 @@ end
 
 reg heff_l, hmsb_l;
 wire zero = heff[3] != heff_l // read the ROM every 8 pixels
-         || (hmsb_l!=hdump[HDUMPW-1] && HJUMP==0); // identify a typical blanking end case
+         || (hmsb_l!=hdump[7] && HJUMP==0); // identify a typical blanking end case
 
 always @(posedge clk) begin
     if( rst ) begin
@@ -148,7 +148,7 @@ always @(posedge clk) begin
         hflip_g  <= 0;
     end else if(pxl_cen) begin
         heff_l <= heff[3];
-        hmsb_l <= hdump[HDUMPW-1];
+        hmsb_l <= hdump[7];
         if( zero ) begin
             rom_cs <= ~rst & blankn;
             rom_addr[0+:VW] <= veff[0+:VW]^{VW{vflip_g}};