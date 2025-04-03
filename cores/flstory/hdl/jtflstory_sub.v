546f754b9a9322ed4cb6e5d4da795262ffc2dbc6
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2025-04-03 20:36:25 +0200

    flstory: Bronx game (#1019)
    - disable sub-cpu busrq
    - add user1 ROM region as IO for the sub CPU

--- cores/flstory/hdl/jtflstory_sub.v
+++ cores/flstory/hdl/jtflstory_sub.v
@@ -36,13 +36,17 @@ module jtflstory_sub(
     // ROM access
     output reg       rom_cs,
     input     [ 7:0] rom_data,
-    input            rom_ok
+    input            rom_ok,
+
+    output reg       user1_cs,
+    input     [ 7:0] user1_data,
+    input            user1_ok
 );
 
 wire [15:0] A;
 reg  [ 7:0] din;
 reg         rst_n;
-wire        mreq_n, rfsh_n, int_n, busak_n;
+wire        mreq_n, iorq_n, m1_n, rfsh_n, int_n, busak_n;
 wire        bus_cen;
 
 assign A        = addr;
@@ -53,15 +57,18 @@ assign bus_cen  = cen & ~bus_wait;
 always @* begin
     rom_cs      = 0;
     bus_cs      = 0;
+    user1_cs    = 0;
 
     if( !mreq_n && rfsh_n ) case(A[15:14])
         0,1,2: rom_cs = 1;
             3: bus_cs = 1;
     endcase
+    if( !iorq_n && m1_n )
+        user1_cs = 1;
 end
 
 always @* begin
-    din = rom_cs ? rom_data : bus_din;
+    din = rom_cs ? rom_data : user1_cs ? user1_data : bus_din;
 end
 
 always @(posedge clk) begin
@@ -77,9 +84,9 @@ jtframe_sysz80 #(.RAM_AW(11),.CLR_INT(1),.RECOVERY(1)) u_cpu(
     .nmi_n      ( nmi_n       ),
     .busrq_n    ( busrq_n     ),
     .busak_n    ( busak_n     ),
-    .m1_n       (             ),
+    .m1_n       ( m1_n        ),
     .mreq_n     ( mreq_n      ),
-    .iorq_n     (             ),
+    .iorq_n     ( iorq_n      ),
     .rd_n       ( rd_n        ),
     .wr_n       ( wr_n        ),
     .rfsh_n     ( rfsh_n      ),
@@ -90,8 +97,8 @@ jtframe_sysz80 #(.RAM_AW(11),.CLR_INT(1),.RECOVERY(1)) u_cpu(
     .ram_dout   (             ),
     // ROM access
     .ram_cs     ( 1'b0        ),
-    .rom_cs     ( rom_cs      ),
-    .rom_ok     ( rom_ok      )
+    .rom_cs     ( rom_cs | user1_cs ),
+    .rom_ok     ( rom_ok | user1_ok )
 );
 
 endmodule
