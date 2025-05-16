ac3da6707fc01dd854c03b0187df6c37ce601a12
Rafael Paiva <rafael.paiva@jotego.es>
2025-05-16 11:14:15 +0200

    Saving VDP memory with ioctl_dump for scenes. Not working yet, and adds sync issues

--- cores/s18/ver/rest2bin.sh
+++ cores/s18/ver/rest2bin.sh
@@ -6,7 +6,9 @@ main(){
 	REST="rest.bin"
 
 	refresh_sdram
+	get_main_mmr
 	substitute_64k_in_sdram
+	get_vdp
 	obtain_mmr_hex
 }
 
@@ -16,6 +18,14 @@ refresh_sdram(){
 
 substitute_64k_in_sdram(){
 	dd if=$REST of=sdram_bank0.bin bs=1 count=$SIZE seek=$VRAM_OFFSET conv=notrunc
+}
+
+get_vdp(){
+	dd if=$REST of=vdp.bin bs=1 count=$SIZE skip=$SIZE conv=notrunc
+}
+
+get_main_mmr(){
+	tail --bytes 8 $REST > main_mmr.bin
 }
 
 obtain_mmr_hex(){
