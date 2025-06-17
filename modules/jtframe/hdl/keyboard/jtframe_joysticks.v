7d8370b299e0b8f65e20202d707de1bfa2eabd38
Rafael Paiva <rafael.paiva@jotego.es>
2025-06-17 17:56:49 +0200

    Character selection for TWIN16 (#1011)

--- modules/jtframe/hdl/keyboard/jtframe_joysticks.v
+++ modules/jtframe/hdl/keyboard/jtframe_joysticks.v
@@ -297,17 +297,17 @@ module jtframe_joy1_pos(
         // move 1P to a different position
         case(pos)
             1: begin
-                joy2 <= raw1;
+                joy2 <= raw1; joy1 <= raw2;
                 start[1] <= raw_start[0];
                 coin [1] <= raw_coin[0];
             end
             2: begin
-                joy3 <= raw1;
+                joy3 <= raw1; joy1 <= raw3;
                 start[2] <= raw_start[0];
                 coin [2] <= raw_coin[0];
             end
             3: begin
-                joy4 <= raw1;
+                joy4 <= raw1; joy1 <= raw4;
                 start[3] <= raw_start[0];
                 coin [3] <= raw_coin[0];
             end
