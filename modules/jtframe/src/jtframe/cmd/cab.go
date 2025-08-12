e4f04b9e4722cc25a32a1b4d07a2eeac8214db7b
Jose Tejada <jose.tejada@jotego.es>
2025-08-12 16:52:20 +0200

    better = parsing

--- modules/jtframe/src/jtframe/cmd/cab.go
+++ modules/jtframe/src/jtframe/cmd/cab.go
@@ -110,8 +110,6 @@ func (cab *cab_converter)make_hexfile(cabfile io.Reader) (hex []byte, e error) {
 				if e!=nil { return nil, fmt.Errorf("%w at line %d",e,cab.linecnt) }
 				if looping {
 					loop_body = append(loop_body,parsed...)
-				} else {
-					cab.frame_cnt += cab.count_lines(parsed)
 				}
 			}
 			case LOOP_START: {
@@ -126,6 +124,7 @@ func (cab *cab_converter)make_hexfile(cabfile io.Reader) (hex []byte, e error) {
 			}
 		}
 		hex=append(hex,parsed...)
+		cab.frame_cnt += cab.count_lines(parsed)
 	}
 	return hex, nil
 }
@@ -166,7 +165,6 @@ func (cab *cab_converter)execute_loop(body []byte,times int) (unwrapped []byte) 
 	for ;times>1;times-- {
 		unwrapped=append(unwrapped,body...)
 	}
-	cab.frame_cnt+=cab.count_lines(unwrapped)
 	return unwrapped
 }
 
@@ -199,7 +197,7 @@ func (cab *cab_converter)parse_tokens( tokens []string ) (parsed []byte, e error
 			default: return nil, fmt.Errorf("Unknown action '%s'",action)
 		}
 	}
-	parsed = make([]byte,0,2*repeat)
+	parsed = make([]byte,0,3*repeat)
 	for ;repeat>0;repeat-- {
 		encoded := fmt.Sprintf("%x\n",value)
 		parsed=append(parsed,[]byte(encoded)...)
@@ -220,7 +218,7 @@ func (cab *cab_converter)calc_repetitions(expr string) (repeat int, valid bool) 
 			msg := fmt.Sprintf("a valid number must be entered after =\nbut found %s at line %d",expr,cab.linecnt)
 			panic(msg)
 		}
-		repeat = final_frame-cab.frame_cnt
+		repeat = final_frame-cab.frame_cnt+1
 		if repeat<0 {
 			msg := fmt.Sprintf("The frame count is already at %d, cannot wait until frame %d (at line %d)",cab.frame_cnt,final_frame,cab.linecnt)
 			panic(msg)
