jotego/jtcores

commit 5fc7758985695a68ae39264a665b8deb752962e4

fixes ioctl_wr pulse shorter than 1 clock cycle in verilator sims

modules/jtframe/hdl/ver/test.cpp
@@ -330,7 +330,7 @@ int fileLength( const char *name ) {
 
 class Download {
     UUT& dut;
-    int addr, din, ticks,len, cart_start, nvram_start;
+    int addr, din, ticks, zeroat, len, cart_start, nvram_start;
     char *buf, *iodin;
     bool done, cart, nvram, full_download, iodump_busy;
     int read_buf() {
@@ -407,7 +407,7 @@ public:
                 fputs("Short ROM download\n",stderr);
             }
         }
-        ticks = 0;
+        ticks = 0; zeroat = 0;
         done = false;
         dut.ioctl_rom = 1;
         dut.ioctl_addr = 0;
@@ -416,7 +416,7 @@ public:
         addr = -1;
     }
     void update() {
-        dut.ioctl_wr = 0;
+        if(ticks==zeroat) dut.ioctl_wr = 0;
         if( dut.ioctl_rom ) step_download();
         if( iodump_busy ) iodump_step();
     }
@@ -447,6 +447,7 @@ public:
                 case 1:
                     if( addr < len ) {
                         dut.ioctl_wr = 1;
+                        zeroat = ticks+2;
                     } else {
 #ifdef _JTFRAME_IOCTL_RD
                         dut.ioctl_ram   = 0;

commit bba7de2669141bc1a1cc006f0c36c096b41e1c91

fixes CPS1 verilator load that got broken in 5fc7758985695a68ae39264a665b8deb752962e4.

cores/cps1/hdl/jtcps1_game.v
@@ -365,6 +365,7 @@ assign snd_addr   = 0;
 assign snd_cs     = 0;
 assign snd_left   = 0;
 assign snd_right  = 0;
+assign snd_peak   = 0;
 assign adpcm_addr = 0;
 assign adpcm_cs   = 0;
 assign sample     = 0;

modules/jtframe/hdl/ver/test.cpp
@@ -416,7 +416,11 @@ public:
         addr = -1;
     }
     void update() {
+#ifdef _JTFRAME_SIM96
         if(ticks==zeroat) dut.ioctl_wr = 0;
+#else
+        dut.ioctl_wr = 0;
+#endif
         if( dut.ioctl_rom ) step_download();
         if( iodump_busy ) iodump_step();
     }