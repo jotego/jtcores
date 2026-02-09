19b56900f699dffda3a3d8a1ee2a0c7a52045718
Frédéric Mahé <frederic.mahe@cirad.fr>
2026-02-09 19:22:11 +0100

    make command has a --quiet option
    ...or `--silent`, or `-s` if you prefer. Very minor, feel free to discard.

--- cores/riders/ver/prot/sim-prot.sh
+++ cores/riders/ver/prot/sim-prot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 verilator prot.v --cc prot.cpp --exe
-make -C obj_dir -f Vprot.mk > /dev/null || echo "make error"
+make --quiet -C obj_dir -f Vprot.mk || echo "make error"
 obj_dir/Vprot
