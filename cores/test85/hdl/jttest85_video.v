21199bc56c7632e6c3c1f46d6fec66814c18fb8b
Jose Tejada <jose.tejada@jotego.es>
2026-05-24 11:23:56 +0200

    minor

--- cores/test85/hdl/jttest85_video.v
+++ cores/test85/hdl/jttest85_video.v
@@ -103,28 +103,3 @@ jttest85_font u_font(
 );
 
 endmodule
-
-module jttest85_font(
-    input             clk,
-    input      [ 9:0] rom_addr,
-    output     [ 7:0] rom_data
-);
-
-wire [ 6:0] ascii     = rom_addr[9:3];
-wire [ 6:0] font_code = ascii < 7'h20 ? 7'd0 : ascii - 7'h20;
-wire [ 9:0] font_addr = { font_code, rom_addr[2:0] };
-
-jtframe_ram #(
-    .AW      ( 10          ),
-    .DW      (  8          ),
-    .SYNFILE ( "font0.hex" )
-) u_font(
-    .clk     ( clk       ),
-    .cen     ( 1'b1      ),
-    .data    ( 8'd0      ),
-    .addr    ( font_addr ),
-    .we      ( 1'b0      ),
-    .q       ( rom_data  )
-);
-
-endmodule
