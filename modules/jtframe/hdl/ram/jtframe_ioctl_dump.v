49818798f1462b050ab85b05944bdd528e1ca014
Jose Tejada <jose.tejada@jotego.es>
2026-06-14 17:06:31 +0200

    feath(cps3): EEPROM support

--- modules/jtframe/hdl/ram/jtframe_ioctl_dump.v
+++ modules/jtframe/hdl/ram/jtframe_ioctl_dump.v
@@ -17,7 +17,7 @@
     Date: 30-8-2023 */
 
 // Use with BRAM blocks to dump them to ioctl_din
-// only works with DW=8 or 16
+// only works with DW=8, 16 or 32
 
 module jtframe_ioctl_dump #(parameter
     DW0=8, DW1=8, DW2=8, DW3=8, DW4=8, DW5=8,
@@ -68,8 +68,19 @@ module jtframe_ioctl_dump #(parameter
     output  [(AW4==0?0:AW4-1):(AW4==0?0:DW4>>4)] addr4_mx,
     output  [(AW5==0?0:AW5-1):(AW5==0?0:DW5>>4)] addr5_mx,
 
-    input      [ 1:0] we0, we1, we2, we3, we4, we5,
-    output     [ 1:0] we0_mx, we1_mx, we2_mx, we3_mx, we4_mx, we5_mx,
+    input      [(DW0>>3)-1:0] we0,
+    input      [(DW1>>3)-1:0] we1,
+    input      [(DW2>>3)-1:0] we2,
+    input      [(DW3>>3)-1:0] we3,
+    input      [(DW4>>3)-1:0] we4,
+    input      [(DW5>>3)-1:0] we5,
+
+    output     [(DW0>>3)-1:0] we0_mx,
+    output     [(DW1>>3)-1:0] we1_mx,
+    output     [(DW2>>3)-1:0] we2_mx,
+    output     [(DW3>>3)-1:0] we3_mx,
+    output     [(DW4>>3)-1:0] we4_mx,
+    output     [(DW5>>3)-1:0] we5_mx,
 
     input      [23:0] ioctl_addr,
     input             ioctl_ram,
@@ -80,19 +91,86 @@ module jtframe_ioctl_dump #(parameter
 
 `ifdef SIMULATION
 initial begin // assertions
-    if( DW0!=8 && DW0!=16 ) begin $display("Bad DW0 size in %m"); $finish; end
-    if( DW1!=8 && DW1!=16 ) begin $display("Bad DW1 size in %m"); $finish; end
-    if( DW2!=8 && DW2!=16 ) begin $display("Bad DW2 size in %m"); $finish; end
-    if( DW3!=8 && DW3!=16 ) begin $display("Bad DW3 size in %m"); $finish; end
-    if( DW4!=8 && DW4!=16 ) begin $display("Bad DW4 size in %m"); $finish; end
-    if( DW5!=8 && DW5!=16 ) begin $display("Bad DW5 size in %m"); $finish; end
+    if( DW0!=8 && DW0!=16 && DW0!=32 ) begin $display("Bad DW0 size in %m"); $finish; end
+    if( DW1!=8 && DW1!=16 && DW1!=32 ) begin $display("Bad DW1 size in %m"); $finish; end
+    if( DW2!=8 && DW2!=16 && DW2!=32 ) begin $display("Bad DW2 size in %m"); $finish; end
+    if( DW3!=8 && DW3!=16 && DW3!=32 ) begin $display("Bad DW3 size in %m"); $finish; end
+    if( DW4!=8 && DW4!=16 && DW4!=32 ) begin $display("Bad DW4 size in %m"); $finish; end
+    if( DW5!=8 && DW5!=16 && DW5!=32 ) begin $display("Bad DW5 size in %m"); $finish; end
 end
 `endif
 
-reg  [ 1:0] we0_io, we1_io, we2_io, we3_io, we4_io, we5_io;
+reg  [(DW0>>3)-1:0] we0_io;
+reg  [(DW1>>3)-1:0] we1_io;
+reg  [(DW2>>3)-1:0] we2_io;
+reg  [(DW3>>3)-1:0] we3_io;
+reg  [(DW4>>3)-1:0] we4_io;
+reg  [(DW5>>3)-1:0] we5_io;
 reg  [ 5:0] sel;
 reg  [23:0] part_addr, ioctl_adl;
 reg  [23:0] offset;
+wire [31:0] dout0_full, dout1_full, dout2_full,
+            dout3_full, dout4_full, dout5_full;
+wire [ 7:0] dout0_byte = byte_sel( dout0_full, ioctl_addr[1:0], DW0 ),
+            dout1_byte = byte_sel( dout1_full, ioctl_addr[1:0], DW1 ),
+            dout2_byte = byte_sel( dout2_full, ioctl_addr[1:0], DW2 ),
+            dout3_byte = byte_sel( dout3_full, ioctl_addr[1:0], DW3 ),
+            dout4_byte = byte_sel( dout4_full, ioctl_addr[1:0], DW4 ),
+            dout5_byte = byte_sel( dout5_full, ioctl_addr[1:0], DW5 );
+wire [ 3:0] we0_next = byte_we( ioctl_addr[1:0], DW0 ),
+            we1_next = byte_we( ioctl_addr[1:0], DW1 ),
+            we2_next = byte_we( ioctl_addr[1:0], DW2 ),
+            we3_next = byte_we( ioctl_addr[1:0], DW3 ),
+            we4_next = byte_we( ioctl_addr[1:0], DW4 ),
+            we5_next = byte_we( ioctl_addr[1:0], DW5 );
+
+assign dout0_full[DW0-1:0] = dout0;
+assign dout1_full[DW1-1:0] = dout1;
+assign dout2_full[DW2-1:0] = dout2;
+assign dout3_full[DW3-1:0] = dout3;
+assign dout4_full[DW4-1:0] = dout4;
+assign dout5_full[DW5-1:0] = dout5;
+
+generate
+    if( DW0<32 ) begin : gen_dout0_extend
+        assign dout0_full[31:DW0] = 0;
+    end
+    if( DW1<32 ) begin : gen_dout1_extend
+        assign dout1_full[31:DW1] = 0;
+    end
+    if( DW2<32 ) begin : gen_dout2_extend
+        assign dout2_full[31:DW2] = 0;
+    end
+    if( DW3<32 ) begin : gen_dout3_extend
+        assign dout3_full[31:DW3] = 0;
+    end
+    if( DW4<32 ) begin : gen_dout4_extend
+        assign dout4_full[31:DW4] = 0;
+    end
+    if( DW5<32 ) begin : gen_dout5_extend
+        assign dout5_full[31:DW5] = 0;
+    end
+endgenerate
+
+function [7:0] byte_sel;
+    input [31:0] data;
+    input [ 1:0] byte_addr;
+    input integer dw;
+begin
+    byte_sel = dw==32 && byte_addr==2'd3 ? data[31:24] :
+               dw==32 && byte_addr==2'd2 ? data[23:16] :
+               dw>=16 && byte_addr[0]    ? data[15: 8] : data[ 7:0];
+end
+endfunction
+
+function [3:0] byte_we;
+    input [ 1:0] byte_addr;
+    input integer dw;
+begin
+    byte_we = dw==32 ? (4'b0001 << byte_addr) :
+              dw==16 ? (byte_addr[0] ? 4'b0010 : 4'b0001) : 4'b0001;
+end
+endfunction
 
 always @(posedge clk) begin
     ioctl_adl <= ioctl_addr;
@@ -116,29 +194,29 @@ assign we5_mx   = ioctl_ram ?  we5_io : we5;
 always @(posedge clk)  begin
     we0_io <= 0;       we1_io <= 0;       we2_io <= 0;
     we3_io <= 0;       we4_io <= 0;       we5_io <= 0;
-    ioctl_din <= sel[0] ? ( (DW0==16 && ioctl_addr[0]) ? dout0[DW0-1 -:8] : dout0[7:0]) :
-                 sel[1] ? ( (DW1==16 && ioctl_addr[0]) ? dout1[DW1-1 -:8] : dout1[7:0]) :
-                 sel[2] ? ( (DW2==16 && ioctl_addr[0]) ? dout2[DW2-1 -:8] : dout2[7:0]) :
-                 sel[3] ? ( (DW3==16 && ioctl_addr[0]) ? dout3[DW3-1 -:8] : dout3[7:0]) :
-                 sel[4] ? ( (DW4==16 && ioctl_addr[0]) ? dout4[DW4-1 -:8] : dout4[7:0]) :
-                 sel[5] ? ( (DW5==16 && ioctl_addr[0]) ? dout5[DW5-1 -:8] : dout5[7:0]) :
+    ioctl_din <= sel[0] ? dout0_byte :
+                 sel[1] ? dout1_byte :
+                 sel[2] ? dout2_byte :
+                 sel[3] ? dout3_byte :
+                 sel[4] ? dout4_byte :
+                 sel[5] ? dout5_byte :
                  ioctl_aux;
     if(ioctl_ram && ioctl_wr) begin
-        if(sel[0]) we0_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW0==8 };
-        if(sel[1]) we1_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW1==8 };
-        if(sel[2]) we2_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW2==8 };
-        if(sel[3]) we3_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW3==8 };
-        if(sel[4]) we4_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW4==8 };
-        if(sel[5]) we5_io <= { ioctl_addr[0], ~ioctl_addr[0] || DW5==8 };
+        if(sel[0]) we0_io <= we0_next[(DW0>>3)-1:0];
+        if(sel[1]) we1_io <= we1_next[(DW1>>3)-1:0];
+        if(sel[2]) we2_io <= we2_next[(DW2>>3)-1:0];
+        if(sel[3]) we3_io <= we3_next[(DW3>>3)-1:0];
+        if(sel[4]) we4_io <= we4_next[(DW4>>3)-1:0];
+        if(sel[5]) we5_io <= we5_next[(DW5>>3)-1:0];
     end
 end
 
-assign din0_mx = ioctl_ram ? {DW0==16?2:1{ioctl_dout}} : din0;
-assign din1_mx = ioctl_ram ? {DW1==16?2:1{ioctl_dout}} : din1;
-assign din2_mx = ioctl_ram ? {DW2==16?2:1{ioctl_dout}} : din2;
-assign din3_mx = ioctl_ram ? {DW3==16?2:1{ioctl_dout}} : din3;
-assign din4_mx = ioctl_ram ? {DW4==16?2:1{ioctl_dout}} : din4;
-assign din5_mx = ioctl_ram ? {DW5==16?2:1{ioctl_dout}} : din5;
+assign din0_mx = ioctl_ram ? {(DW0>>3){ioctl_dout}} : din0;
+assign din1_mx = ioctl_ram ? {(DW1>>3){ioctl_dout}} : din1;
+assign din2_mx = ioctl_ram ? {(DW2>>3){ioctl_dout}} : din2;
+assign din3_mx = ioctl_ram ? {(DW3>>3){ioctl_dout}} : din3;
+assign din4_mx = ioctl_ram ? {(DW4>>3){ioctl_dout}} : din4;
+assign din5_mx = ioctl_ram ? {(DW5>>3){ioctl_dout}} : din5;
 
 always @(*) begin
     sel    = 0;
