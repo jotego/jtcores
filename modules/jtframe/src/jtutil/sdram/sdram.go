52bb1a4effe9113845f6c0f65ea94f17c020bfbb
Jose Tejada <jose.tejada@jotego.es>
2026-06-19 17:11:21 +0200

    fix(jtutil sdram): better support for header.offset in TOML

--- modules/jtframe/src/jtutil/sdram/sdram.go
+++ modules/jtframe/src/jtutil/sdram/sdram.go
@@ -487,20 +487,15 @@ func bankOffset(regCnt int, hinfo mra.HeaderOffset, rom []byte) ([]int, []string
 		}
 	}
 	// Final values from header (if defined).
-	for k := 1; k < len(hinfo.Regions); k++ {
-		idx := hinfo.Start + (k << 1)
-		if idx+1 >= len(rom) {
-			return nil, nil, fmt.Errorf("wrong header: offset index %d for %s is outside ROM length %X", idx, hinfo.Regions[k], len(rom))
+	headerOffsets, err := readHeaderOffsets(hinfo, rom, header)
+	if err != nil {
+		return nil, nil, err
+	}
+	for k := 1; k < len(headerOffsets); k++ {
+		if k >= len(offsets) {
+			break
 		}
-		pos := (int(rom[idx]) << 8) | int(rom[idx+1])
-		if hinfo.Reverse {
-			pos = (int(rom[idx+1]) << 8) | int(rom[idx])
-		}
-		pos <<= hinfo.Bits
-		if pos+header > len(rom) {
-			return nil, nil, fmt.Errorf("wrong header: computed offset %X for %s exceeds ROM length %X", pos+header, hinfo.Regions[k], len(rom))
-		}
-		offsets[k] = pos + header
+		offsets[k] = headerOffsets[k]
 		if verbose {
 			fmt.Printf("%-4d %-20s %X\n", k, hinfo.Regions[k], offsets[k])
 		}
@@ -513,6 +508,26 @@ func bankOffset(regCnt int, hinfo mra.HeaderOffset, rom []byte) ([]int, []string
 		fmt.Println()
 	}
 	return offsets, hinfo.Regions, nil
+}
+
+func readHeaderOffsets(hinfo mra.HeaderOffset, rom []byte, header int) ([]int, error) {
+	offsets := make([]int, len(hinfo.Regions))
+	for k := range hinfo.Regions {
+		idx := hinfo.Start + (k << 1)
+		if idx+1 >= len(rom) {
+			return nil, fmt.Errorf("wrong header: offset index %d for %s is outside ROM length %X", idx, hinfo.Regions[k], len(rom))
+		}
+		pos := (int(rom[idx]) << 8) | int(rom[idx+1])
+		if hinfo.Reverse {
+			pos = (int(rom[idx+1]) << 8) | int(rom[idx])
+		}
+		pos <<= hinfo.Bits
+		if pos+header > len(rom) {
+			return nil, fmt.Errorf("wrong header: computed offset %X for %s exceeds ROM length %X", pos+header, hinfo.Regions[k], len(rom))
+		}
+		offsets[k] = pos + header
+	}
+	return offsets, nil
 }
 
 func parseMemConfig(core string) (*mem.MemConfig, error) {
@@ -761,10 +776,13 @@ func dump(name string, rom []byte, p0, p1, lim, fill int) (int, error) {
 	}
 	if p1 == p0 && fill == 0 {
 		err := os.Remove(name)
-		if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("WARNING: empty region file %s does not exist\n", name)
+		} else if err != nil {
 			return 0, err
+		} else {
+			fmt.Println("Removed file", name)
 		}
-		fmt.Println("Removed file", name)
 		return p1, nil
 	}
 	if err := os.WriteFile(name, rom[p0:p1], 0664); err != nil {
