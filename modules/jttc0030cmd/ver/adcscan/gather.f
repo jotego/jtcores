fbb6002e52ebee655532b4cc0ebf46e9fec79809
Andrea Bogazzi <andreabogazzi79@gmail.com>
2026-08-17 10:04:43 +0200

    jttc0030cmd: restart ADC conversion on ANM write (fixes AN4-AN7 scan)

---
+++ modules/jttc0030cmd/ver/adcscan/gather.f
@@ -0,0 +1,8 @@
+../../hdl/jttc0030cmd.v
+../../hdl/IKA87AD_mnemonics.sv
+../../hdl/IKA87AD_primitives.sv
+../../hdl/IKA87AD_opdec.sv
+../../hdl/IKA87AD_microcode.sv
+../../hdl/IKA87AD.sv
+../../../jtframe/hdl/ram/jtframe_prom.v
+../../../jtframe/hdl/ram/jtframe_dual_ram.v
