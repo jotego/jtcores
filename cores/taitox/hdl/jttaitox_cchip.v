b31e351ccbed053e1144a771dc846843b389a88e
Andrea Bogazzi <andreabogazzi79@gmail.com>
2026-08-21 13:18:34 +0200

    more bram in sdram

--- cores/taitox/hdl/jttaitox_cchip.v
+++ cores/taitox/hdl/jttaitox_cchip.v
@@ -41,10 +41,19 @@ module jttaitox_cchip(
     // coin counters / lockouts (pc_out), for the coin door
     output     [ 7:0] counters,
 
+`ifdef RAM_IN_SDRAM
+    // Mask ROM + game EPROM as one 12 kB SDRAM region: mask at 0x0000,
+    // EPROM at 0x1000. Saves 12 BRAM blocks on the small devices.
+    output     [13:0] ccrom_addr,
+    output            ccrom_cs,
+    input      [ 7:0] ccrom_data,
+    input             ccrom_ok
+`else
     output     [11:0] cchip_mask_addr,
     input      [ 7:0] cchip_mask_data,
     output     [12:0] cchip_eprom_addr,
     input      [ 7:0] cchip_eprom_data
+`endif
 );
 
 reg [7:0] cc_pa, cc_pb, cc_an;
@@ -55,10 +64,18 @@ always @(posedge clk) begin
     cc_an <= { tilt, 4'hf, service, coin[1], coin[0] };
 end
 
+`ifdef RAM_IN_SDRAM
+// Stall the MCU while a fetch is in flight. The address is combinational on
+// the MCU state, so it stays put while cen is held low.
+wire mcu_cen = cen & ~(ccrom_cs & ~ccrom_ok);
+`else
+wire mcu_cen = cen;
+`endif
+
 jttc0030cmd u_cchip(
     .rst        ( rst              ),
     .clk        ( clk              ),
-    .cen        ( cen              ),
+    .cen        ( mcu_cen          ),
     .cs         ( cs               ),
     .addr       ( addr             ),
     .din        ( din              ),
@@ -74,10 +91,21 @@ jttc0030cmd u_cchip(
     .pb_out     (                  ),
     .pc_out     ( counters         ),
     .an         ( cc_an            ),
+`ifdef RAM_IN_SDRAM
+    .mrom_addr  (                  ),
+    .mrom_data  ( ccrom_data       ),
+    .eprom_addr (                  ),
+    .eprom_data ( ccrom_data       ),
+    .rom_addr   ( ccrom_addr       ),
+    .rom_cs     ( ccrom_cs         ),
+`else
     .mrom_addr  ( cchip_mask_addr  ),
     .mrom_data  ( cchip_mask_data  ),
     .eprom_addr ( cchip_eprom_addr ),
     .eprom_data ( cchip_eprom_data ),
+    .rom_addr   (                  ),
+    .rom_cs     (                  ),
+`endif
     // debug (unused)
     .dbg_pc     (                  ),
     .dbg_fetch  (                  )
--- cores/taitox/hdl/jttaitox_colmix.v
+++ cores/taitox/hdl/jttaitox_colmix.v
@@ -34,7 +34,10 @@ module jttaitox_colmix(
     output     [ 4:0] blue
 );
 
-localparam [8:0] BACKDROP = 9'h1f0;   // palette index where the bg color is stored.
+// palette index where the bg color is stored.
+// this is part of seta xi-002(1?) but for now we don't have a model for it.
+// this is part of the seta investigation.
+localparam [8:0] BACKDROP = 9'h1f0;
 
 reg  [ 8:0] col;
 reg  [14:0] rgb;
